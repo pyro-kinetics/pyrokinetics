f1c717561137382f72a0705d17d9051cdd2a3f38
Bhavin Patel <bhav.patel@ukaea.uk>
2021-05-21 15:13:19 +0200

    Update tests to match ne template file names

--- pyrokinetics/test/test_cgyro.py
+++ pyrokinetics/test/test_cgyro.py
@@ -4,16 +4,15 @@ import os
 home = os.environ['HOME']
 base = home+'/pyrokinetics/pyrokinetics/templates/'
 
-pyro = Pyro(gkFile=base+'input.cgyro', gkType='CGYRO')
+pyro = Pyro(gkFile=base+'test_gs2.cgyro', gkType='CGYRO')
 
 mil = pyro.mil
-
-print(mil['shat'])
-mil['shat'] = 2.3
-
-print(mil['shat'])
 
 flags =  {'THETA_PLOT' : 32 }
 
 pyro.addFlags(flags)
-pyro.writeSingle(filename='test_cgyro.in')
+pyro.writeSingle(filename='test_cgyro.cgyro')
+
+pyro.setOutputCode('GS2')
+pyro.writeSingle(filename='test_cgyro.gs2')
+
