f78a7e584527029670850986f9272e3b21bf7fea
Bhavin Patel <15210802+bpatel2107@users.noreply.github.com>
2021-06-02 16:18:12 +0100

    Converted value settings to attr rather than dict for cgyro

--- pyrokinetics/cgyro.py
+++ pyrokinetics/cgyro.py
@@ -5,12 +5,15 @@ from .numerics import Numerics
 from .gk_code import GKCode
 import os
 from path import Path
+from cleverdict import CleverDict
+
 
 class CGYRO(GKCode):
     """
     Basic CGYRO object inheriting method from GKCode
 
     """
+
     def __init__(self):
 
         self.base_template_file = os.path.join(Path(__file__).dirname(), 'templates', 'input.cgyro')
@@ -74,10 +77,11 @@ class CGYRO(GKCode):
         beta_prime_scale = cgyro['BETA_STAR_SCALE']
 
         if pyro.local_geometry_type == 'Miller':
-            if pyro.local_geometry['Bunit'] is not None:
-                pyro.local_geometry['beta_prime'] = - pyro.local_species['a_lp'] / pyro.local_geometry['Bunit'] ** 2 * beta_prime_scale
+            if pyro.local_geometry.Bunit is not None:
+                pyro.local_geometry.beta_prime = - pyro.local_species.a_lp / pyro.local_geometry.Bunit ** 2 * \
+                                                 beta_prime_scale
             else:
-                pyro.local_geometry['beta_prime'] = 0.0
+                pyro.local_geometry.beta_prime = 0.0
         else:
             raise NotImplementedError
 
@@ -99,9 +103,9 @@ class CGYRO(GKCode):
             cgyro_input['EQUILIBRIUM_MODEL'] = 2
 
             # Reference B field - Bunit = q/r dpsi/dr
-            b_ref = miller['Bunit']
+            b_ref = miller.Bunit
 
-            shat = miller['shat']
+            shat = miller.shat
 
             # Assign Miller values to input file
             pyro_cgyro_miller = self.pyro_to_code_miller()
@@ -114,39 +118,39 @@ class CGYRO(GKCode):
 
         # Kinetic data
         local_species = pyro.local_species
-        cgyro_input['N_SPECIES'] = local_species['nspec']
+        cgyro_input['N_SPECIES'] = local_species.nspec
 
-        for i_sp, name in enumerate(local_species['names']):
+        for i_sp, name in enumerate(local_species.names):
             pyro_cgyro_species = self.pyro_to_code_species(i_sp + 1)
 
             for pyro_key, cgyro_key in pyro_cgyro_species.items():
                 cgyro_input[cgyro_key] = local_species[name][pyro_key]
 
-        cgyro_input['NU_EE'] = local_species['electron']['nu']
+        cgyro_input['NU_EE'] = local_species.electron.nu
 
         beta = 0.0
         beta_prime_scale = 1.0
 
         # If species are defined calculate beta and beta_prime_scale
-        if local_species['nref'] is not None:
+        if local_species.nref is not None:
 
-            pref = local_species['nref'] * local_species['tref'] * electron_charge
+            pref = local_species.nref * local_species.tref * electron_charge
 
-            pe = pref * local_species['electron']['dens'] * local_species['electron']['temp']
+            pe = pref * local_species.electron.dens * local_species.electron.temp
 
-            beta = pe/b_ref**2 * 8 * pi * 1e-7
+            beta = pe / b_ref ** 2 * 8 * pi * 1e-7
 
             # Find BETA_STAR_SCALE from beta and p_prime
             if pyro.local_geometry_type == 'Miller':
-                beta_prime_scale = - miller['beta_prime'] / (beta * local_species['a_lp']) * \
-                                   (miller['B0']/miller['Bunit'])**2
+                beta_prime_scale = - miller.beta_prime / (beta * local_species.a_lp) * \
+                                   (miller.B0 / miller.Bunit) ** 2
 
         # Calculate beta from existing value from input
         else:
             if pyro.local_geometry_type == 'Miller':
-                if miller['Bunit'] is not None:
-                    beta = 1.0/miller['Bunit']**2
-                    beta_prime_scale = - miller['beta_prime'] / (beta * local_species['a_lp'])
+                if miller.Bunit is not None:
+                    beta = 1.0 / miller.Bunit ** 2
+                    beta_prime_scale = - miller.beta_prime / (beta * local_species.a_lp)
                 else:
                     beta = 0.0
                     beta_prime_scale = 1.0
@@ -158,23 +162,23 @@ class CGYRO(GKCode):
         # Numerics
         numerics = pyro.numerics
 
-        if numerics['nonlinear']:
+        if numerics.nonlinear:
             cgyro_input['NONLINEAR_FLAG'] = 1
-            cgyro_input['N_RADIAL'] = numerics['nkx']
-            cgyro_input['BOX_SIZE'] = int((numerics['ky'] * 2 * pi * shat / numerics['kx'])+0.1)
+            cgyro_input['N_RADIAL'] = numerics.nkx
+            cgyro_input['BOX_SIZE'] = int((numerics.ky * 2 * pi * shat / numerics.kx) + 0.1)
         else:
             cgyro_input['NONLINEAR_FLAG'] = 0
-            cgyro_input['N_RADIAL'] = numerics['nperiod'] * 2
+            cgyro_input['N_RADIAL'] = numerics.nperiod * 2
             cgyro_input['BOX_SIZE'] = 1
 
-        cgyro_input['KY'] = numerics['ky']
-        cgyro_input['N_TOROIDAL'] = numerics['nky']
+        cgyro_input['KY'] = numerics.ky
+        cgyro_input['N_TOROIDAL'] = numerics.nky
 
-        cgyro_input['N_THETA'] = numerics['ntheta']
-        cgyro_input['THETA_PLOT'] = numerics['ntheta']
+        cgyro_input['N_THETA'] = numerics.ntheta
+        cgyro_input['THETA_PLOT'] = numerics.ntheta
 
-        cgyro_input['N_ENERGY'] = numerics['nenergy']
-        cgyro_input['N_XI'] = numerics['npitch']
+        cgyro_input['N_ENERGY'] = numerics.nenergy
+        cgyro_input['N_XI'] = numerics.npitch
 
         cgyro_input['FIELD_PRINT_FLAG'] = 1
         cgyro_input['MOMENT_PRINT_FLAG'] = 1
@@ -259,19 +263,19 @@ class CGYRO(GKCode):
         for key, val in pyro_cgyro_miller.items():
             miller[key] = cgyro[val]
 
-        miller['kappri'] = miller['s_kappa'] * miller['kappa'] / miller['rho']
-        miller['tri'] = np.arcsin(miller['delta'])
+        miller.kappri = miller.s_kappa * miller.kappa / miller.rho
+        miller.tri = np.arcsin(miller.delta)
 
         beta = cgyro['BETAE_UNIT']
 
         # Assume pref*8pi*1e-7 = 1.0
         if beta != 0:
-            miller['Bunit'] = 1/(beta**0.5)
+            miller.Bunit = 1 / (beta ** 0.5)
             bunit_over_b0 = miller.get_bunit_over_b0()
-            miller['B0'] = miller['Bunit']/bunit_over_b0
+            miller.B0 = miller.Bunit / bunit_over_b0
         else:
-            miller['Bunit'] = None
-            miller['B0'] = None
+            miller.Bunit = None
+            miller.B0 = None
 
     def load_local_species(self, pyro, cgyro):
         """
@@ -282,9 +286,9 @@ class CGYRO(GKCode):
 
         # Dictionary of local species parameters
         local_species = LocalSpecies()
-        local_species['nspec'] = nspec
-        local_species['nref'] = None
-        local_species['names'] = []
+        local_species.nspec = nspec
+        local_species.nref = None
+        local_species.names = []
 
         ion_count = 0
 
@@ -295,49 +299,48 @@ class CGYRO(GKCode):
         for i_sp in range(nspec):
 
             pyro_cgyro_species = self.pyro_to_code_species(i_sp + 1)
-            species_data = {}
+            species_data = CleverDict()
             for p_key, c_key in pyro_cgyro_species.items():
-
                 species_data[p_key] = cgyro[c_key]
 
-            species_data['vel'] = 0.0
-            species_data['a_lv'] = 0.0
+            species_data.vel = 0.0
+            species_data.a_lv = 0.0
 
-            if species_data['z'] == -1:
+            if species_data.z == -1:
                 name = 'electron'
-                species_data['nu'] = cgyro['NU_EE']
-                te = species_data['temp']
-                ne = species_data['dens']
-                me = species_data['mass']
+                species_data.nu = cgyro['NU_EE']
+                te = species_data.temp
+                ne = species_data.dens
+                me = species_data.mass
             else:
-                ion_count+=1
+                ion_count += 1
                 name = f'ion{ion_count}'
 
-            pressure += species_data['temp']*species_data['dens']
-            a_lp += species_data['temp']*species_data['dens'] * (species_data['a_lt'] + species_data['a_ln'])
+            pressure += species_data.temp * species_data.dens
+            a_lp += species_data.temp * species_data.dens * (species_data.a_lt + species_data.a_ln)
 
-            species_data['name'] = name
+            species_data.name = name
 
             # Add individual species data to dictionary of species
             local_species[name] = species_data
-            local_species['names'].append(name)
+            local_species.names.append(name)
 
         # CGYRO beta_prime scale
-        local_species['pressure'] = pressure
-        local_species['a_lp'] = a_lp / (ne * te)
+        local_species.pressure = pressure
+        local_species.a_lp = a_lp / (ne * te)
 
         # Get collision frequency of ion species
         nu_ee = cgyro['NU_EE']
 
         for ion in range(ion_count):
-            key = f'ion{ion+1}'
+            key = f'ion{ion + 1}'
 
             nion = local_species[key]['dens']
             tion = local_species[key]['temp']
             mion = local_species[key]['mass']
 
             # Not exact at log(Lambda) does change but pretty close...
-            local_species[key]['nu'] = nu_ee * (nion / tion**1.5 / mion**0.5) / (ne / te**1.5 /me**0.5)
+            local_species[key]['nu'] = nu_ee * (nion / tion ** 1.5 / mion ** 0.5) / (ne / te ** 1.5 / me ** 0.5)
 
         # Add local_species
         pyro.local_species = local_species
@@ -349,17 +352,17 @@ class CGYRO(GKCode):
         """
 
         pyro_cgyro_param = {
-            'rho' : 'RMIN',
-            'Rmaj' : 'RMAJ',
-            'Rgeo' : 'RMAJ',
-            'q' : 'Q',
-            'kappa' : 'KAPPA',
-            's_kappa' : 'S_KAPPA',
-            'delta' : 'DELTA',
-            's_delta' : 'S_DELTA',
-            'shat' :  'S',
-            'shift' : 'SHIFT',
-            }
+            'rho': 'RMIN',
+            'Rmaj': 'RMAJ',
+            'Rgeo': 'RMAJ',
+            'q': 'Q',
+            'kappa': 'KAPPA',
+            's_kappa': 'S_KAPPA',
+            'delta': 'DELTA',
+            's_delta': 'S_DELTA',
+            'shat': 'S',
+            'shift': 'SHIFT',
+        }
 
         return pyro_cgyro_param
 
@@ -376,7 +379,7 @@ class CGYRO(GKCode):
             'temp': f'TEMP_{iSp}',
             'a_lt': f'DLNTDR_{iSp}',
             'a_ln': f'DLNNDR_{iSp}',
-            }
+        }
 
         return pyro_cgyro_species
 
@@ -396,49 +399,48 @@ class CGYRO(GKCode):
 
         numerics = Numerics()
 
-        numerics['ky'] = cgyro['KY']
+        numerics.ky = cgyro['KY']
 
         try:
-            numerics['nky'] = cgyro['N_TOROIDAL']
-        except  KeyError:
-            numerics['nky'] = 1
-
-        try:
-            numerics['theta0'] = cgyro['PX0'] * 2 * pi
+            numerics.nky = cgyro['N_TOROIDAL']
         except KeyError:
-            numerics['theta0'] = 0.0
+            numerics.nky = 1
 
         try:
-            numerics['nkx'] = cgyro['N_RADIAL']
+            numerics.theta0 = cgyro['PX0'] * 2 * pi
         except KeyError:
-            numerics['nkx'] = 1
+            numerics.theta0 = 0.0
 
-        numerics['nperiod'] = int(cgyro['N_RADIAL'] / 2)
+        try:
+            numerics.nkx = cgyro['N_RADIAL']
+        except KeyError:
+            numerics.nkx = 1
 
-        shat = pyro.local_geometry['shat']
+        numerics.nperiod = int(cgyro['N_RADIAL'] / 2)
+
+        shat = pyro.local_geometry.shat
 
         try:
             box_size = cgyro['BOX_SIZE']
         except KeyError:
             box_size = 1
 
-        numerics['kx'] = numerics['ky'] * 2 * pi * shat / box_size
+        numerics.kx = numerics.ky * 2 * pi * shat / box_size
 
         try:
-            numerics['ntheta'] = cgyro['N_THETA']
+            numerics.ntheta = cgyro['N_THETA']
         except KeyError:
-            numerics['ntheta'] = 24
+            numerics.ntheta = 24
 
         try:
-            numerics['nenergy'] = cgyro['N_ENERGY']
+            numerics.nenergy = cgyro['N_ENERGY']
         except KeyError:
-            numerics['nenergy'] = 8
+            numerics.nenergy = 8
 
         try:
-            numerics['npitch'] = cgyro['N_XI']
+            numerics.npitch = cgyro['N_XI']
         except KeyError:
-            numerics['npitch'] = 16
-
+            numerics.npitch = 16
 
         try:
             nl_mode = cgyro['NONLINEAR_FLAG']
@@ -446,8 +448,8 @@ class CGYRO(GKCode):
             nl_mode = 0
 
         if nl_mode == 1:
-            numerics['nonlinear'] = True
+            numerics.nonlinear = True
         else:
-            numerics['nonlinear'] = False
+            numerics.nonlinear = False
 
         pyro.numerics = numerics
