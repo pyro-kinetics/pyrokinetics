ef10a9b6f2b35a1f671585f0acbfff30877b9d68
Daniel Kennedy <daniel.kennedy@ukaea.uk>
2021-06-03 12:39:23 +0100

    first draft of gene implementation

---
+++ pyrokinetics/examples/example_gene.py
@@ -0,0 +1,18 @@
+import pyrokinetics
+import os
+import sys
+
+
+templates = os.path.join('..', 'pyrokinetics', 'pyrokinetics', 'templates')
+
+gene_template = os.path.join(templates, 'input.gene')
+
+pyro = pyrokinetics.Pyro(gk_file=gene_template, gk_type='GENE')
+
+pyro.write_gk_file(file_name='test_gene.gene')
+
+pyro.gk_code = 'GS2'
+pyro.write_gk_file(file_name='test_gene.gs2')
+
+pyro.gk_code = 'CGYRO'
+pyro.write_gk_file(file_name='test_gene.cgyro')
