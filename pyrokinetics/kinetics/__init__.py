088114609565e463ad084639703b38d4bb8133df
Bhavin Patel <15210802+bpatel2107@users.noreply.github.com>
2022-02-22 17:43:57 +0000

    Flake8 fixes __init__.py

--- pyrokinetics/kinetics/__init__.py
+++ pyrokinetics/kinetics/__init__.py
@@ -1,5 +1,8 @@
+# Import main Kinetics class
+from .Kinetics import Kinetics
+
 # Import KineticsReaders
-from .KineticsReader import KineticsReader, kinetics_readers
+from .KineticsReader import kinetics_readers
 from .KineticsReaderSCENE import KineticsReaderSCENE
 from .KineticsReaderJETTO import KineticsReaderJETTO
 from .KineticsReaderTRANSP import KineticsReaderTRANSP
@@ -9,5 +12,4 @@ kinetics_readers["SCENE"] = KineticsReaderSCENE
 kinetics_readers["JETTO"] = KineticsReaderJETTO
 kinetics_readers["TRANSP"] = KineticsReaderTRANSP
 
-# Import main Kinetics class
-from .Kinetics import Kinetics
+__all__ = ["Kinetics"]
