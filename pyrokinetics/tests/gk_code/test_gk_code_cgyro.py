fccbe4de997da0d234f14a18d3d969277e6f6c81
Liam Pattinson <liampattinson@gmail.com>
2022-02-28 14:48:11 +0000

    Minor bug fixes, black reformatting

--- pyrokinetics/tests/gk_code/test_gk_code_cgyro.py
+++ pyrokinetics/tests/gk_code/test_gk_code_cgyro.py
@@ -2,19 +2,18 @@ from pyrokinetics.gk_code import GKCodeCGYRO
 from pyrokinetics import template_dir
 import pytest
 
-class TestGKCodeCGYRO:
 
+class TestGKCodeCGYRO:
     @pytest.fixture
     def cgyro(self):
         return GKCodeCGYRO()
 
-    def test_verify(self,cgyro):
+    def test_verify(self, cgyro):
         """Ensure that 'verify' does not raise exception on CGYRO file"""
-        cgyro.verify( template_dir.joinpath("input.cgyro"))
+        cgyro.verify(template_dir.joinpath("input.cgyro"))
 
-    @pytest.mark.parametrize( "filename", ["input.gs2","input.gene","transp.cdf"])
-    def test_verify_bad_inputs(self,cgyro,filename):
+    @pytest.mark.parametrize("filename", ["input.gs2", "input.gene", "transp.cdf"])
+    def test_verify_bad_inputs(self, cgyro, filename):
         """Ensure that 'verify' raises exception on non-CGYRO file"""
         with pytest.raises(Exception):
-            cgyro.verify( template_dir.joinpath(filename))
-
+            cgyro.verify(template_dir.joinpath(filename))
