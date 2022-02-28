8ba2b742e66fc673c61861dd8209a6945ef12330
Liam Pattinson <liampattinson@gmail.com>
2022-02-28 14:48:11 +0000

    Minor bug fixes, black reformatting

--- pyrokinetics/tests/gk_code/test_gk_code_gene.py
+++ pyrokinetics/tests/gk_code/test_gk_code_gene.py
@@ -2,19 +2,18 @@ from pyrokinetics.gk_code import GKCodeGENE
 from pyrokinetics import template_dir
 import pytest
 
-class TestGKCodeGENE:
 
+class TestGKCodeGENE:
     @pytest.fixture
     def gene(self):
         return GKCodeGENE()
 
-    def test_verify(self,gene):
+    def test_verify(self, gene):
         """Ensure that 'verify' does not raise exception on GENE file"""
-        gene.verify( template_dir.joinpath("input.gene"))
+        gene.verify(template_dir.joinpath("input.gene"))
 
-    @pytest.mark.parametrize( "filename", ["input.cgyro","input.gs2","transp.cdf"])
-    def test_verify_bad_inputs(self,gene,filename):
+    @pytest.mark.parametrize("filename", ["input.cgyro", "input.gs2", "transp.cdf"])
+    def test_verify_bad_inputs(self, gene, filename):
         """Ensure that 'verify' raises exception on non-GENE file"""
         with pytest.raises(Exception):
-            gene.verify( template_dir.joinpath(filename))
-
+            gene.verify(template_dir.joinpath(filename))
--- pyrokinetics/tests/gk_code/test_gk_code_gs2.py
+++ pyrokinetics/tests/gk_code/test_gk_code_gs2.py
@@ -2,19 +2,18 @@ from pyrokinetics.gk_code import GKCodeGS2
 from pyrokinetics import template_dir
 import pytest
 
-class TestGKCodeGS2:
 
+class TestGKCodeGS2:
     @pytest.fixture
     def gs2(self):
         return GKCodeGS2()
 
-    def test_verify(self,gs2):
+    def test_verify(self, gs2):
         """Ensure that 'verify' does not raise exception on GS2 file"""
-        gs2.verify( template_dir.joinpath("input.gs2"))
+        gs2.verify(template_dir.joinpath("input.gs2"))
 
-    @pytest.mark.parametrize( "filename", ["input.cgyro","input.gene","transp.cdf"])
-    def test_verify_bad_inputs(self,gs2,filename):
+    @pytest.mark.parametrize("filename", ["input.cgyro", "input.gene", "transp.cdf"])
+    def test_verify_bad_inputs(self, gs2, filename):
         """Ensure that 'verify' raises exception on non-GS2 file"""
         with pytest.raises(Exception):
-            gs2.verify( template_dir.joinpath(filename))
-
+            gs2.verify(template_dir.joinpath(filename))
