8b6493ae0e247e885d92b35fef0cd691a2cb78fa
Liam Pattinson <liampattinson@gmail.com>
2022-03-01 17:58:16 +0000

    Moving to system in which GKCode is split into several classes
    GKCode should be split into several classes, one dealing with
    reading input files, one with writing input files, and one for
    handling GKOutput (which itself may need to be split into
    several subclasses).
    GKInputReaders should read gk code input files (GS2, CGYRO, GENE),
    and provide functions for producing Numerics, LocalGeometry,
    and LocalSpecies. Ultimately, Pyro objects should use these to
    generate such objects.

---
+++ pyrokinetics/gk_code/GKInputReader.py
@@ -0,0 +1,81 @@
+import f90nml
+from abc import abstractmethod
+from typing import Optional
+
+from ..typing import PathLike
+from ..readers import Reader, create_reader_factory
+from ..local_geometry import LocalGeometry
+from ..local_species import LocalSpecies
+from ..numerics import Numerics
+
+
+class GKInputReader(Reader):
+    """
+    Base for classes that read gyrokinetics codes' input files and
+    produce Numerics, LocalGeometry, and LocalSpecies objects.
+    """
+
+    def __init__(self, filename: Optional[PathLike] = None):
+        if filename is not None:
+            read(filename)
+
+    @abstractmethod
+    def read(self, filename: PathLike) -> None:
+        """
+        Reads in GK input file to store as internal dictionary
+        """
+        self.data = f90nml.read(filename).todict()
+
+    def read_template(self) -> None:
+        """
+        Reads in GK template file to store as internal dictionary
+        """
+        return self.read(self.base_template_file)
+
+    @abstractmethod
+    def verify(self, filename):
+        """
+        Ensure file is valid for a given GK input type.
+        """
+        pass
+
+    @abstractmethod
+    def is_nonlinear(self) -> bool:
+        """
+        Return true if the GKCode is nonlinear, otherwise return False
+        """
+        pass
+
+    def is_linear(self) -> bool:
+        return not is_nonlinear(self)
+
+    @abstractmethod
+    def add_flags(self, flags) -> None:
+        """
+        Add extra flags to a GK code input file
+
+        """
+        pass
+
+    @abstractmethod
+    def get_local_geometry(self) -> LocalGeometry:
+        """
+        Load local geometry object from a GK code input file
+        """
+        pass
+
+    @abstractmethod
+    def get_local_species(self) -> LocalSpecies:
+        """
+        Load local species object from a GK code input file
+        """
+        pass
+
+    @abstractmethod
+    def get_numerics(self, shat: float) -> Numerics:
+        """
+        Gather numerical info (grid spacing, time steps, etc)
+        """
+        pass
+
+gk_input_readers = create_reader_factory(BaseReader=GKInputReader)
