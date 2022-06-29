16cc59ec61c924464e81a467b27c8040383a2d00
Liam Pattinson <liampattinson@gmail.com>
2022-06-29 15:25:52 +0100

    Added golden answer tests for GKOutputReaders

--- pyrokinetics/tests/gk_code/test_gk_output_reader_gene.py
+++ pyrokinetics/tests/gk_code/test_gk_output_reader_gene.py
@@ -1,12 +1,13 @@
 from pyrokinetics.gk_code import GKOutputReaderGENE
 from pyrokinetics import template_dir
 from pathlib import Path
+import numpy as np
 import pytest
 import subprocess
 
-# FIXME Currently not testing most of this class! So far only testing that 'verify' works
-#      as intended, as going beyond this will require both a minimal set of test files
-#      of size <<1MB, and a complex mocking setup.
+from pyrokinetics.tests.gk_code.utils import array_similar, get_golden_answer_data
+
+# TODO mock output tests, similar to GS2
 
 
 @pytest.fixture(scope="module")
@@ -94,3 +95,57 @@ def test_infer_path_from_input_file_gene(input_path):
         assert output_path == Path(f"dir/to/parameters_{last_4_chars}")
     else:
         assert output_path == Path("dir/to/")
+
+
+# Golden answer tests
+# Compares against results obtained using GKCode methods from commit 7d551eaa
+# This data was gathered from templates/outputs/GENE_linear
+
+reference_data_commit_hash = "7d551eaa"
+
+
+@pytest.fixture(scope="class")
+def golden_answer_reference_data(request):
+    this_dir = Path(__file__).parent
+    cdf_path = (
+        this_dir
+        / "golden_answers"
+        / f"gene_linear_output_{reference_data_commit_hash}.netcdf4"
+    )
+    ds = get_golden_answer_data(cdf_path)
+    request.cls.reference_data = ds
+
+
+@pytest.fixture(scope="class")
+def golden_answer_data(request):
+    path = template_dir / "outputs" / "GENE_linear" / "parameters_0001"
+    request.cls.data = GKOutputReaderGENE().read(path)
+
+
+@pytest.mark.usefixtures("golden_answer_reference_data", "golden_answer_data")
+class TestGENEGoldenAnswers:
+    def test_coords(self):
+        """
+        Ensure that all reference coords are present in data
+        """
+        for c in self.reference_data.coords:
+            dtype = self.reference_data[c].dtype
+            if dtype == "float64" or dtype == "complex128":
+                assert array_similar(self.reference_data[c], self.data[c])
+            else:
+                assert np.array_equal(self.reference_data[c], self.data[c])
+
+    @pytest.mark.parametrize(
+        "var",
+        [
+            "fields",
+            "fluxes",
+            "eigenvalues",
+            "eigenfunctions",
+            "growth_rate",
+            "growth_rate_tolerance",
+            "mode_frequency",
+        ],
+    )
+    def test_data_vars(self, var):
+        assert array_similar(self.reference_data[var], self.data[var])
