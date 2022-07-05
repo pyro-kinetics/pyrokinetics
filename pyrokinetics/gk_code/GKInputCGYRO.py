d8e476b374c79176cd0da99580eb825237783bdf
Bhavin Patel <15210802+bpatel2107@users.noreply.github.com>
2022-07-05 12:11:09 +0100

    Fix error in CGYRO Input set

--- pyrokinetics/gk_code/GKInputCGYRO.py
+++ pyrokinetics/gk_code/GKInputCGYRO.py
@@ -276,8 +276,7 @@ class GKInputCGYRO(GKInput):
         local_geometry: LocalGeometry,
         local_species: LocalSpecies,
         numerics: Numerics,
-        local_norm,
-        LocalNorm,
+        local_norm: LocalNorm,
         template_file: Optional[PathLike] = None,
         **kwargs,
     ):
