68a67e0e3aaba4e954c155cf7468dd2af47a4ab9
Peter Hill <peter.hill@york.ac.uk>
2022-07-06 11:13:33 +0100

    Convert TGLF reader to new GKInput API

--- pyrokinetics/gk_code/__init__.py
+++ pyrokinetics/gk_code/__init__.py
@@ -3,15 +3,15 @@ from .GKOutputReader import GKOutputReader, gk_output_readers, get_growth_rate_t
 from .GKInputGS2 import GKInputGS2
 from .GKInputCGYRO import GKInputCGYRO
 from .GKInputGENE import GKInputGENE
+from .GKInputTGLF import GKInputTGLF
 from .GKOutputReaderGS2 import GKOutputReaderGS2
 from .GKOutputReaderCGYRO import GKOutputReaderCGYRO
 from .GKOutputReaderGENE import GKOutputReaderGENE
-from .GKCodeTGLF import GKCodeTGLF
 
-gk_inputs["TGLF"] = GKCodeTGLF
 gk_inputs["GS2"] = GKInputGS2
 gk_inputs["CGYRO"] = GKInputCGYRO
 gk_inputs["GENE"] = GKInputGENE
+gk_inputs["TGLF"] = GKInputTGLF
 gk_output_readers["GS2"] = GKOutputReaderGS2
 gk_output_readers["CGYRO"] = GKOutputReaderCGYRO
 gk_output_readers["GENE"] = GKOutputReaderGENE
