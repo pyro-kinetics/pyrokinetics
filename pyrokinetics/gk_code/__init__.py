cd43c1bbe01ea9ad1663798dc96bee332e1355f0
Bhavin Patel <15210802+bpatel2107@users.noreply.github.com>
2022-07-07 16:21:43 +0100

    Added TGLF output reader and update example script

--- pyrokinetics/gk_code/__init__.py
+++ pyrokinetics/gk_code/__init__.py
@@ -7,6 +7,7 @@ from .GKInputTGLF import GKInputTGLF
 from .GKOutputReaderGS2 import GKOutputReaderGS2
 from .GKOutputReaderCGYRO import GKOutputReaderCGYRO
 from .GKOutputReaderGENE import GKOutputReaderGENE
+from .GKOutputReaderTGLF import GKOutputReaderTGLF
 
 gk_inputs["GS2"] = GKInputGS2
 gk_inputs["CGYRO"] = GKInputCGYRO
@@ -15,3 +16,4 @@ gk_inputs["TGLF"] = GKInputTGLF
 gk_output_readers["GS2"] = GKOutputReaderGS2
 gk_output_readers["CGYRO"] = GKOutputReaderCGYRO
 gk_output_readers["GENE"] = GKOutputReaderGENE
+gk_output_readers["TGLF"] = GKOutputReaderTGLF
