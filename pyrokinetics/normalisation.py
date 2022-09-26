Fix normalisations for mixed simulation units

pyrokinetics/normalisation.py
@@ -163,16 +163,27 @@ def _create_unit_registry(simulation_context: pint.Context) -> pint.UnitRegistry
     # can always convert between them -- so we create a context to
     # convert between the dimensions, and immediately enable it
     vref_context = pint.Context("vref")
-    vref_context.add_transformation(
-        "[vref_nrl_dim]",
-        "[vref_most_probable_dim]",
-        lambda ureg, x: x * np.sqrt(2) * ureg.vref_ratio_units,
-    )
-    vref_context.add_transformation(
-        "[vref_most_probable_dim]",
-        "[vref_nrl_dim]",
-        lambda ureg, x: x / (np.sqrt(2) * ureg.vref_ratio_units),
-    )
+
+    # Ok this is pretty horrible, but pint can't convert between
+    # dimensions if there are other units involved, so we need to
+    # explicitly construct all those possible conversions. Let's
+    # assume for now that we're only interested in vref/lref
+    # combinations
+    possible_mixed_units = [""] + [
+        f"/ lref_{kind}" for kind in REFERENCE_CONVENTIONS["lref"]
+    ]
+    for mixed_unit in possible_mixed_units:
+        vref_context.add_transformation(
+            f"[vref_nrl_dim] {mixed_unit}",
+            f"[vref_most_probable_dim] {mixed_unit}",
+            lambda ureg, x: x * np.sqrt(2) * ureg.vref_ratio_units,
+        )
+        vref_context.add_transformation(
+            f"[vref_most_probable_dim] {mixed_unit}",
+            f"[vref_nrl_dim] {mixed_unit}",
+            lambda ureg, x: x / (np.sqrt(2) * ureg.vref_ratio_units),
+        )
+    vref_context
     ureg.add_context(vref_context)
     ureg.enable_contexts(vref_context)
 
@@ -362,6 +373,18 @@ class SimulationNormalisation:
             self.pyrokinetics.lref,
             lambda ureg, x: x.m * self.pyrokinetics.lref,
         )
+        self.context.add_transformation(
+            "[vref_nrl_dim]",
+            self.pyrokinetics.vref,
+            lambda ureg, x: x.m * self.pyrokinetics.vref,
+        )
+
+        # Mixed unit conversion because pint
+        self.context.add_transformation(
+            "[vref_nrl_dim] / [lref_minor_radius_dim]",
+            self.pyrokinetics.vref / self.pyrokinetics.lref,
+            lambda ureg, x: x.m * self.pyrokinetics.vref / self.pyrokinetics.lref,
+        )
 
     def set_kinetic_references(self, kinetics: Kinetics, psi_n: float):
         """Set the temperature, density, and mass reference values for
@@ -385,6 +408,13 @@ class SimulationNormalisation:
             "[mref_deuterium_dim]",
             self.pyrokinetics.mref,
             lambda ureg, x: x.m * self.pyrokinetics.mref,
+        )
+
+        # Mixed unit conversion because pint
+        self.context.add_transformation(
+            "[nref_electron_dim] * [tref_electron_dim]",
+            self.pyrokinetics.nref * self.pyrokinetics.tref,
+            lambda ureg, x: x.m * self.pyrokinetics.nref * self.pyrokinetics.tref,
         )
 
 
@@ -462,13 +492,14 @@ class ConventionNormalisation:
             f"vref_{self.name}": 1.0,
         }
 
-        self._system.base_units["bref_B0"] = {f"bref_{self.name}": 1.0}
-        self._system.base_units["lref_minor_radius"] = {f"lref_{self.name}": 1.0}
-        self._system.base_units["mref_deuterium"] = {f"mref_{self.name}": 1.0}
-        self._system.base_units["nref_electron"] = {f"nref_{self.name}": 1.0}
-        self._system.base_units["qref_elementary_charge"] = {f"qref_{self.name}": 1.0}
-        self._system.base_units["tref_electron"] = {f"tref_{self.name}": 1.0}
-        self._system.base_units["vref_nrl"] = {f"vref_{self.name}": 1.0}
+        # For all of our possible base simulation units, we should be
+        # able to convert to this convention's base units. These are
+        # all straight-forward, because we're not mixing dimensions
+        for dimension, kinds in REFERENCE_CONVENTIONS.items():
+            for kind in kinds:
+                self._system.base_units[f"{dimension}_{kind}"] = {
+                    f"{dimension}_{self.name}": 1.0
+                }
 
         # getattr rather than []-indexing as latter returns a quantity
         # rather than a unit (??)

pyrokinetics/tests/test_normalisation.py
@@ -109,7 +109,18 @@ def test_normalisation_constructor(geometry, kinetics):
     assert np.isclose(velocity_gs2, expected)
 
 
-def test_convert_to_normalisation(geometry, kinetics):
+def test_convert_single_unit_to_normalisation(geometry, kinetics):
+    norm = SimulationNormalisation(
+        "test", geometry=geometry, kinetics=kinetics, psi_n=0.5
+    )
+
+    length_gs2 = 1 * norm.gs2.lref
+    length_gene = length_gs2.to(norm.gene)
+    expected_gene = 0.5 * norm.gene.lref
+    assert np.isclose(length_gene, expected_gene)
+
+
+def test_convert_mixed_units_to_normalisation(geometry, kinetics):
     norm = SimulationNormalisation(
         "test", geometry=geometry, kinetics=kinetics, psi_n=0.5
     )
@@ -126,7 +137,7 @@ def test_convert_to_normalisation(geometry, kinetics):
     assert np.isclose(frequency_gene, expected_gene)
 
 
-def test_convert_simulation_to_physical(geometry, kinetics):
+def test_convert_single_units_simulation_to_physical(geometry, kinetics):
     """Convert directly to physical reference unit"""
     norm = SimulationNormalisation(
         "test", geometry=geometry, kinetics=kinetics, psi_n=0.5
@@ -139,7 +150,7 @@ def test_convert_simulation_to_physical(geometry, kinetics):
     assert length_physical == length_expected
 
 
-def test_convert_simulation_to_normalisation(geometry, kinetics):
+def test_convert_single_units_simulation_to_normalisation(geometry, kinetics):
     """Convert to physical reference unit using norm object"""
     norm = SimulationNormalisation(
         "test", geometry=geometry, kinetics=kinetics, psi_n=0.5
@@ -154,3 +165,20 @@ def test_convert_simulation_to_normalisation(geometry, kinetics):
     length_gene = length.to(norm.gene)
     length_gene_expected = 0.5 * norm.gene.lref
     assert length_gene == length_gene_expected
+
+
+def test_convert_mixed_units_to_normalisation(geometry, kinetics):
+    norm = SimulationNormalisation(
+        "test", geometry=geometry, kinetics=kinetics, psi_n=0.5
+    )
+
+    frequency_gs2 = 1 * norm.units.vref_most_probable / norm.units.lref_minor_radius
+
+    frequency = frequency_gs2.to(norm)
+    expected = (1 / np.sqrt(2)) * norm.vref / norm.lref
+
+    frequency_gene = frequency.to(norm.gene)
+    expected_gene = (2 / np.sqrt(2)) * norm.gene.vref / norm.gene.lref
+
+    assert np.isclose(frequency, expected)
+    assert np.isclose(frequency_gene, expected_gene)

pyrokinetics/tests/test_roundtrip.py
@@ -13,12 +13,15 @@ def assert_close_or_equal(name, left, right, norm=None):
     ):
         assert left == right, f"{name}: {left} != {right}"
     else:
-        # FIXME: To be removed when everything is a Quantity
-        if isinstance(left, ureg.Quantity):
-            left = left.magnitude
-        if isinstance(right, ureg.Quantity):
-            right = right.magnitude
-        assert np.allclose(left, right), f"{name}: {left} != {right}"
+        if norm and not isinstance(right, float):
+            try:
+                assert np.allclose(
+                    left, right.to(norm)
+                ), f"{name}: {left} != {right.to(norm)}"
+            except pint.DimensionalityError:
+                raise ValueError(f"Failure: {name}, {left} != {right}")
+        else:
+            assert np.allclose(left, right), f"{name}: {left} != {right}"
 
 
 @pytest.fixture(scope="module")
@@ -94,19 +97,22 @@ def test_compare_roundtrip(setup_roundtrip, gk_code):
 
     assert pyro.local_species.keys() == code.local_species.keys()
 
-    for key in pyro.local_species.keys():
-        if key in pyro.local_species["names"]:
-            for field in species_fields:
+    with pyro.norms.units.as_system(pyro.norms.pyrokinetics), pyro.norms.units.context(
+        pyro.norms.context
+    ):
+        for key in pyro.local_species.keys():
+            if key in pyro.local_species["names"]:
+                for field in species_fields:
+                    assert_close_or_equal(
+                        f"{code.gk_code} {key}.{field}",
+                        pyro.local_species[key][field],
+                        code.local_species[key][field],
+                        pyro.norms,
+                    )
+            else:
                 assert_close_or_equal(
-                    f"{code.gk_code} {key}.{field}",
-                    pyro.local_species[key][field],
-                    code.local_species[key][field],
+                    f"{code.gk_code} {key}",
+                    pyro.local_species[key],
+                    code.local_species[key],
                     pyro.norms,
                 )
-        else:
-            assert_close_or_equal(
-                f"{code.gk_code} {key}",
-                pyro.local_species[key],
-                code.local_species[key],
-                pyro.norms,
-            )