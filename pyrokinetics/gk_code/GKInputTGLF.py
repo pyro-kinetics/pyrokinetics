03c36ff48773d6ae0d412a3b354659dafd49bcfa
Peter Hill <peter.hill@york.ac.uk>
2022-10-19 14:22:19 +0100

    Remove unused import

--- pyrokinetics/gk_code/GKInputTGLF.py
+++ pyrokinetics/gk_code/GKInputTGLF.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from cleverdict import CleverDict
 from typing import Dict, Any, Optional
 from ..typing import PathLike
-from ..constants import pi, electron_charge
+from ..constants import pi
 from ..local_species import LocalSpecies
 from ..local_geometry import (
     LocalGeometry,
