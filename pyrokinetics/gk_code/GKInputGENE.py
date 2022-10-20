aabcdda51b384080af839706b90c54c72dea64a5
Bhavin Patel <15210802+bpatel2107@users.noreply.github.com>
2022-10-20 16:15:16 +0100

    Update pyrokinetics/gk_code/GKInputGENE.py
    Co-authored-by: Peter Hill <peter.hill@york.ac.uk>

--- pyrokinetics/gk_code/GKInputGENE.py
+++ pyrokinetics/gk_code/GKInputGENE.py
@@ -78,7 +78,7 @@ class GKInputGENE(GKInput):
         super().write(filename, float_format=float_format)
 
     def is_nonlinear(self) -> bool:
-        return bool(self.data["general"].get("nonlinear", 0))
+        return self.data["general"].get("nonlinear", False)
 
     def add_flags(self, flags) -> None:
         """
