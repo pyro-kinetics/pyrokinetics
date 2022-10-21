e0e6215a0dd5ae2c7c6a859a10fa2c47b312c40d
Bhavin Patel <15210802+bpatel2107@users.noreply.github.com>
2022-10-21 19:58:43 +0100

    Example script switching between geometries

--- pyrokinetics/examples/example_geqdsk.py
+++ pyrokinetics/examples/example_geqdsk.py
@@ -2,7 +2,6 @@ from pyrokinetics import Pyro, template_dir
 
 # Equilibrium file
 eq_file = template_dir / "test.geqdsk"
-# eq_file = "spr45_600x900.eqdsk"
 # Kinetics data file
 kinetics_file = template_dir / "scene.cdf"
 
@@ -17,11 +16,11 @@ pyro = Pyro(
 # Generate local Miller parameters at psi_n=0.5
 pyro.load_local_geometry(psi_n=0.5, local_geometry="Fourier", show_fit=True)
 
-pyro.switch_local_geometry(local_geometry="Miller", show_fit=True)
-
 pyro.switch_local_geometry(local_geometry="MXH", show_fit=True)
 
 pyro.switch_local_geometry(local_geometry="FourierCGYRO", show_fit=True)
+
+pyro.switch_local_geometry(local_geometry="Miller", show_fit=True)
 
 pyro.load_local_species(psi_n=0.5)
 
