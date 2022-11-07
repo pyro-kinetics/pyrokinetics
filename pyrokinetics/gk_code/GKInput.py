ad32365a40eab47dda012aac44746697e06a5d52
Liam Pattinson <liampattinson@gmail.com>
2022-11-07 16:28:21 +0000

    Fixed bugs related to force_ndarray for units
    monkeypatching f90nml Namelists to convert numpy scalars to their
    underlying types

--- pyrokinetics/gk_code/GKInput.py
+++ pyrokinetics/gk_code/GKInput.py
@@ -12,6 +12,23 @@ from ..local_species import LocalSpecies
 from ..numerics import Numerics
 from ..normalisation import SimulationNormalisation as Normalisation
 
+# Monkeypatch on f90nml Namelists to autoconvert numpy scalar arrays to their
+# underlying types and drop units.
+
+_f90repr_orig = f90nml.Namelist._f90repr
+
+
+def _f90repr_patch(self, val):
+    if isinstance(val, np.ndarray) and not val.shape:
+        if hasattr(val, "units"):
+            val = val.magnitude[()]
+        else:
+            val = val[()]
+    return _f90repr_orig(self, val)
+
+
+f90nml.Namelist._f90repr = _f90repr_patch
+
 
 class GKInput(Reader):
     """
