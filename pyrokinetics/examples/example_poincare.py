214ba4a3f73752570541e3fa5a341ef7238a8d12
mgiacomin <mgiacomin@users.noreply.github.com>
2023-02-23 12:06:15 +0000

    [skip ci] Apply black changes

--- pyrokinetics/examples/example_poincare.py
+++ pyrokinetics/examples/example_poincare.py
@@ -29,7 +29,5 @@ ntot = nturns * yarray.shape[0]
 colorlist = plt.cm.jet(np.linspace(0, 1, xarray.shape[0]))
 plt.figure()
 for i, color in enumerate(colorlist):
-    plt.plot(coords[0, :, :, i].ravel(),
-             coords[1, :, :, i].ravel(),
-             '.', color=color)
+    plt.plot(coords[0, :, :, i].ravel(), coords[1, :, :, i].ravel(), ".", color=color)
 plt.show()
