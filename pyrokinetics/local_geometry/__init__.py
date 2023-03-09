62e4fa02df8604df621790d8672f781f98e03f55
Bhavin Patel <15210802+bpatel2107@users.noreply.github.com>
2023-03-09 09:03:47 +0000

    Added basicmiller to init

--- pyrokinetics/local_geometry/__init__.py
+++ pyrokinetics/local_geometry/__init__.py
@@ -1,5 +1,6 @@
 from .LocalGeometry import LocalGeometry, local_geometries
 from .LocalGeometryMiller import LocalGeometryMiller, default_miller_inputs
+from .LocalGeometryBasicMiller import LocalGeometryBasicMiller, default_basic_miller_inputs
 from .LocalGeometryFourierGENE import (
     LocalGeometryFourierGENE,
     default_fourier_gene_inputs,
@@ -12,12 +13,14 @@ from .LocalGeometryMXH import LocalGeometryMXH, default_mxh_inputs
 
 # Register LocalGeometry objects with factory
 local_geometries["Miller"] = LocalGeometryMiller
+local_geometries["BasicMiller"] = LocalGeometryBasicMiller
 local_geometries["FourierGENE"] = LocalGeometryFourierGENE
 local_geometries["MXH"] = LocalGeometryMXH
 local_geometries["FourierCGYRO"] = LocalGeometryFourierCGYRO
 
 __all__ = [
     "LocalGeometry",
+    "default_basic_miller_inputs",
     "default_miller_inputs",
     "default_fourier_gene_inputs",
     "default_mxh_inputs",
