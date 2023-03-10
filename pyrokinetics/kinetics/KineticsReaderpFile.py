8ea2a42aacad9ad1baab8026411e113283958ae6
jfparisi <jparisi@pppl.gov>
2023-03-10 14:03:23 -0500

    Bug fixes.

--- pyrokinetics/kinetics/KineticsReaderpFile.py
+++ pyrokinetics/kinetics/KineticsReaderpFile.py
@@ -1,37 +1,22 @@
 """
 Reads in an Osborne pFile: https://omfit.io/_modules/omfit_classes/omfit_osborne.html#OMFITosborneProfile
 
-<<<<<<< HEAD
-Install OMFIT classes with:
-
-pip install --upgrade omfit_classes
 
 """
-=======
-Uses modified pFile reader from eqtools.py.
-'''
->>>>>>> de16051 (Adding in-house pFile reader adapted from eqtools)
 from typing import Dict
 from ..typing import PathLike
 from .KineticsReader import KineticsReader
 from ..species import Species
 from ..constants import electron_mass, hydrogen_mass, deuterium_mass
-from ..pfile_reader import PFileReader
+from .pfile_reader import PFileReader
+from pyrokinetics.equilibrium.equilibrium import read_equilibrium
 
 import numpy as np
 from scipy.interpolate import InterpolatedUnivariateSpline, interp1d
-<<<<<<< HEAD
-from omfit_classes.omfit_osborne import (
-    OMFITpFile,
-)  # Robust pFile reader from OMFIT. See https://omfit.io/_modules/omfit_classes/omfit_osborne.html#OMFITosborneProfile.
-from omfit_classes.omfit_eqdsk import OMFITgeqdsk
 import copy
-
-=======
 import csv
 import re
 from collections import namedtuple
->>>>>>> de16051 (Adding in-house pFile reader adapted from eqtools)
 
 def ion_species_selector(nucleons, charge):
     """
@@ -69,44 +54,6 @@ def np_to_T(n, p):
     """
     return np.divide(p, n) / ((1.381e-23) * (1e19) * (11600) * (1000))
 
-<<<<<<< HEAD
-
-def remap_osborne(
-    pFile_data_in, points="ne", **kw
-):  #### copying from omfit, as bug in my installation.
-    mapped = copy.deepcopy(pFile_data_in)
-    keys = mapped.keys()
-
-    if isinstance(points, str):
-        remap_psin = pFile_data_in[points]["psinorm"]
-
-    elif type(points) is int:  # Evenly spaced grid rebase
-        remap_psin = np.linspace(0, 1, points)
-
-    elif type(points) in [list, tuple, np.ndarray]:  # user provided grid rebase
-        assert np.all(np.abs(points) <= 1), "All values must be on the interval [0, 1]"
-        assert np.all(
-            np.diff(points) > 0
-        ), "All values must be monotonically increasing"
-        remap_psin = points
-
-    mapped["psinorm"] = remap_psin  # save the psin grid
-    # iterate through all keys
-    for key in keys:
-        if key in ("N Z A",):  # this is not mapped to psi so skip it
-            continue
-        f_key = interp1d(pFile_data_in[key]["psinorm"], pFile_data_in[key]["data"])
-        f_key_derivative = interp1d(
-            pFile_data_in[key]["psinorm"], pFile_data_in[key]["derivative"], **kw
-        )
-        mapped[key]["psinorm"] = remap_psin
-        mapped[key]["data"] = f_key(remap_psin)
-        mapped[key]["derivative"] = f_key_derivative(remap_psin)
-
-    return mapped
-
-=======
->>>>>>> de16051 (Adding in-house pFile reader adapted from eqtools)
 
 class KineticsReaderpFile(KineticsReader):
     def read(
@@ -119,27 +66,12 @@ class KineticsReaderpFile(KineticsReader):
         """
         # Read pFile, get generic data.
 
-<<<<<<< HEAD
-        pFile = OMFITpFile(str(filename))
-        ne = remap_osborne(
-            pFile, "ne"
-        )  # remap to get pFile on same uniform grid for all entries.
-        te = remap_osborne(pFile, "te")
-        psi_n = ne["ne"]["psinorm"]
-
-        electron_temp_data = te["te"]["data"]
-        electron_dens_data = ne["ne"]["data"]
-=======
-        #pFile = OMFITpFile(str(filename))
         pFile = PFileReader(str(filename))
-        ne = remap_osborne(pFile,'ne') # remap to get pFile on same uniform grid for all entries.
-        te = remap_osborne(pFile,'te')
         
         psi_n = pFile.__getattribute__('ne').x
 
         electron_temp_data = pFile.__getattribute__('ne').y
         electron_dens_data = pFile.__getattribute__('te').y
->>>>>>> de16051 (Adding in-house pFile reader adapted from eqtools)
 
         electron_temp_func = InterpolatedUnivariateSpline(
             psi_n, electron_temp_data
@@ -149,12 +81,11 @@ class KineticsReaderpFile(KineticsReader):
         geqdsk_filename = (
             str(filename)[:-5] + "geqdsk"
         )  # Important: geqdsk is in same directory as pFile, and is called geqdsk.
-        # How can we get rho = r/a? For now, we read a geqdsk file to get r/a and psinorm.
-        rminor_geqdsk = (
-            OMFITgeqdsk(geqdsk_filename)["fluxSurfaces"]["avg"]["a"] / 2
-        )  # This is the half-diameter of each flux surface.
-        psinorm_geqdsk = OMFITgeqdsk(geqdsk_filename)["AuxQuantities"]["PSI_NORM"]
-        # we can also get rho_t = sqrt(toroidal flux_norm): rhonorm_geqdsk = omfitgeqdsk(geqdsk_filename)['AuxQuantities']['RHO']
+
+        geqdsk_equilibrium = read_equilibrium(geqdsk_filename) # Better way to do this, using the gfile that is actually read in.
+
+        rminor_geqdsk = geqdsk_equilibrium["r_minor"].values
+        psinorm_geqdsk = geqdsk_equilibrium["psi_n"].values
         rho_geqdsk = rminor_geqdsk / rminor_geqdsk[-1]
 
         # We next interpolate find rho_func for the pFile. This is probably overkill.
@@ -162,18 +93,10 @@ class KineticsReaderpFile(KineticsReader):
         rho_pFile = rho_geqdsk_interp(psi_n)
         rho_func = InterpolatedUnivariateSpline(psi_n, rho_pFile)
 
-<<<<<<< HEAD
-        try:
-            omega = remap_osborne(pFile, "omega")
-            omega_data = omega["omega"]["data"]
-        except Exception:
-            omega_data = np.zeros(len(psi_n), dtype="float")
-=======
-        if 'omeg' in pFile_params:
+        if 'omeg' in pFile._params:
             omega_data = pFile.__getattribute__('omeg').y
         else:
             omega_data = np.zeros(len(psi_n), dtype = 'float')
->>>>>>> de16051 (Adding in-house pFile reader adapted from eqtools)
 
         omega_func = InterpolatedUnivariateSpline(psi_n, omega_data)
 
@@ -189,35 +112,19 @@ class KineticsReaderpFile(KineticsReader):
 
         result = {"electron": electron}
 
-<<<<<<< HEAD
-        num_ions = len(pFile["N Z A"]["Z"])
-
-        ## Check whether fast particles.
-        fast_particle = 0
-        if "nb" in pFile:
-=======
         num_ions = pFile.__getattribute__('ions').nions
 
         ## Check whether fast particles.
         fast_particle = 0
-        if 'nb' in pFile_params:
->>>>>>> de16051 (Adding in-house pFile reader adapted from eqtools)
+        if 'nb' in pFile._params:
             fast_particle = 1
             print("Fast particles present in pFile \n.")
 
         num_thermal_ions = num_ions - fast_particle
 
         # thermal ions have same temperature in pFile.
-<<<<<<< HEAD
-        ti = remap_osborne(pFile, "ti")
-        ion_temp_data = ti["ti"]["data"]
-        ion_temp_func = InterpolatedUnivariateSpline(
-            psi_n, ion_temp_data
-        )  # Interpolate on psi_n.
-=======
         ion_temp_data = pFile.__getattribute__('ti').y
         ion_temp_func = InterpolatedUnivariateSpline(psi_n, ion_temp_data) # Interpolate on psi_n.
->>>>>>> de16051 (Adding in-house pFile reader adapted from eqtools)
 
         for ion_it in np.arange(num_thermal_ions):
             """
@@ -232,14 +139,7 @@ class KineticsReaderpFile(KineticsReader):
             """
 
             if ion_it == num_thermal_ions - 1:  # Main ion.
-                ni = remap_osborne(pFile, "ni")
-                ion_dens_data = ni["ni"]["data"]
 
-<<<<<<< HEAD
-                charge = pFile["N Z A"]["Z"][ion_it]
-                nucleons = pFile["N Z A"]["A"][ion_it]
-                mass = pFile["N Z A"]["A"][ion_it] * hydrogen_mass
-=======
                 ion_dens_data = pFile.__getattribute__('ni').y
 
                 charge = pFile.__getattribute__('ions').Z[ion_it]
@@ -267,7 +167,6 @@ class KineticsReaderpFile(KineticsReader):
                 charge = pFile.__getattribute__('ions').Z[ion_it]
                 nucleons = pFile.__getattribute__('ions').A[ion_it]
                 mass = nucleons * hydrogen_mass
->>>>>>> de16051 (Adding in-house pFile reader adapted from eqtools)
 
                 species_name = ion_species_selector(nucleons, charge)
                 ion_dens_func = InterpolatedUnivariateSpline(psi_n, ion_dens_data)
@@ -282,64 +181,38 @@ class KineticsReaderpFile(KineticsReader):
                     rho=rho_func,
                 )
 
-            else:  # Impurities.
-                ni = remap_osborne(pFile, "nz{}".format(int(ion_it + 1)))
-                ion_dens_data = ni["nz{}".format(int(ion_it + 1))]["data"]
-
-<<<<<<< HEAD
-                charge = pFile["N Z A"]["Z"][ion_it]
-                nucleons = pFile["N Z A"]["A"][ion_it]
-                mass = pFile["N Z A"]["A"][ion_it] * hydrogen_mass
-
-                species_name = ion_species_selector(nucleons, charge)
-                ion_dens_func = InterpolatedUnivariateSpline(psi_n, ion_dens_data)
-
-                result[species_name] = Species(
-                    species_type=species_name,
-                    charge=charge,
-                    mass=mass,
-                    dens=ion_dens_func,
-                    temp=ion_temp_func,
-                    ang=omega_func,
-                    rho=rho_func,
-                )
+        #print(species_name,charge, mass, ion_dens_func, ion_temp_func, omega_func, rho_func) # Debug.
 
         if fast_particle:  # Adding the fast particle species.
-            nb = remap_osborne(pFile, "nb")
-            fast_ion_dens_data = ni["nb"]["data"]
-            fast_ion_press_data = ni["pb"]["data"]
-=======
             fast_ion_dens_data = pFile.__getattribute__('nb').y
             fast_ion_press_data = pFile.__getattribute__('pb').y
->>>>>>> de16051 (Adding in-house pFile reader adapted from eqtools)
 
-            # estimate fast particle temperature from pressure and density. Very approximate.
-            fast_ion_temp_data = np_to_T(fast_ion_dens_data, fast_ion_press_data)
+            proceed = 1
+            if np.sum(fast_ion_dens_data) < 0.01:
+                print('Fast ion density empty. Not reading fast ions.')
+                proceed = 0
+            if proceed == 1:
+                # estimate fast particle temperature from pressure and density. Very approximate.
+                fast_ion_temp_data = np_to_T(fast_ion_dens_data, fast_ion_press_data)
 
-            fast_ion_dens_func = InterpolatedUnivariateSpline(psi_n, fast_ion_dens_data)
-            fast_ion_temp_func = InterpolatedUnivariateSpline(psi_n, fast_ion_temp_data)
+                fast_ion_dens_func = InterpolatedUnivariateSpline(psi_n, fast_ion_dens_data)
+                fast_ion_temp_func = InterpolatedUnivariateSpline(psi_n, fast_ion_temp_data)
 
-<<<<<<< HEAD
-            charge = pFile["N Z A"]["Z"][-1]
-            nucleons = pFile["N Z A"]["A"][-1]
-            mass = pFile["N Z A"]["A"][-1] * hydrogen_mass
-=======
-            charge = pFile.__getattribute__('ions').Z[-1]
-            nucleons = pFile.__getattribute__('ions').A[-1]
-            mass = nucleons * hydrogen_mass
->>>>>>> de16051 (Adding in-house pFile reader adapted from eqtools)
+                charge = pFile.__getattribute__('ions').Z[-1]
+                nucleons = pFile.__getattribute__('ions').A[-1]
+                mass = nucleons * hydrogen_mass
 
-            fast_species = ion_species_selector(nucleons, charge) + str("_fast")
+                fast_species = ion_species_selector(nucleons, charge) + str("_fast")
 
-            result[fast_species] = Species(
-                species_type=fast_species,
-                charge=charge,
-                mass=mass,
-                dens=fast_ion_dens_func,
-                temp=fast_ion_temp_func,
-                ang=omega_func,
-                rho=rho_func,
-            )
+                result[fast_species] = Species(
+                    species_type=fast_species,
+                    charge=charge,
+                    mass=mass,
+                    dens=fast_ion_dens_func,
+                    temp=fast_ion_temp_func,
+                    ang=omega_func,
+                    rho=rho_func,
+                )
 
         print("result is {} \n".format(result))
 
