8918d9d3e9aec5a28543a36607f535909cfc120f
bpatel2107 <bpatel2107@users.noreply.github.com>
2023-03-30 14:01:01 +0000

    [skip ci] Apply black changes

--- pyrokinetics/diagnostics/Diagnostics.py
+++ pyrokinetics/diagnostics/Diagnostics.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.interpolate import RectBivariateSpline
 import xrft
-import xarray # noqa
+import xarray  # noqa
 import pint  # noqa
 import pint_xarray  # noqa
 from ..pyro import Pyro
@@ -82,9 +82,7 @@ class Diagnostics:
             )
         if self.pyro.gk_input.is_linear():
             raise RuntimeError("Poincare only available for nonlinear runs")
-        apar = self.pyro.gk_output["apar"].sel(
-            time=time, method="nearest"
-        )
+        apar = self.pyro.gk_output["apar"].sel(time=time, method="nearest")
         apar = apar.pint.dequantify()
         kx = apar.kx.values
         ky = apar.ky.values
