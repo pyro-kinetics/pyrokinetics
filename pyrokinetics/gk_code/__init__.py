6142d81b672adb38f39d85b4f2bfe59df0d1fa8a
Bhavin Patel <15210802+bpatel2107@users.noreply.github.com>
2023-04-28 16:57:13 +0100

    Updated gk outputs to use latest dataclass initialiser

--- pyrokinetics/gk_code/__init__.py
+++ pyrokinetics/gk_code/__init__.py
@@ -9,5 +9,24 @@ gk_inputs["CGYRO"] = GKInputCGYRO
 gk_inputs["GENE"] = GKInputGENE
 gk_inputs["TGLF"] = GKInputTGLF
 
-from .gk_output import GKOutput  # noqa
+from .gk_output import GKOutput, supported_gk_output_types  # noqa
 from .GKOutputReaderGS2 import GKOutputReaderGS2  # noqa
+from .GKOutputReaderCGYRO import GKOutputReaderCGYRO  # noqa
+from .GKOutputReaderGENE import GKOutputReaderGENE  # noqa
+from .GKOutputReaderTGLF import GKOutputReaderTGLF  # noqa
+
+#from . import GKOutputReaderGS2
+#from . import GKOutputReaderCGYRO
+#from . import GKOutputReaderGENE
+#from . import GKOutputReaderTGLF
+
+__all__ = [
+    "GKInput",
+    "gk_inputs",
+    "GKOutput",
+    "supported_gk_output_types"
+]
+#gk_output_readers["GS2"] = GKOutputReaderGS2
+#gk_output_readers["CGYRO"] = GKOutputReaderCGYRO
+#gk_output_readers["GENE"] = GKOutputReaderGENE
+#gk_output_readers["TGLF"] = GKOutputReaderTGLF
