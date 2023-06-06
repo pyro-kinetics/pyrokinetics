9162059b07c1e3c53df3fb804dac0b3ae967e8e6
Bhavin Patel <15210802+bpatel2107@users.noreply.github.com>
2023-06-06 12:00:40 +0100

    Update Pfile reader to use Freeqdsk and units

--- pyrokinetics/kinetics/KineticsReaderpFile.py
+++ pyrokinetics/kinetics/KineticsReaderpFile.py
@@ -8,15 +8,14 @@ from ..typing import PathLike
 from .KineticsReader import KineticsReader
 from ..species import Species
 from ..constants import electron_mass, hydrogen_mass, deuterium_mass
-from .pfile_reader import PFileReader
 from pyrokinetics.equilibrium.equilibrium import read_equilibrium
+from ..units import ureg as units, UnitSpline
 
 import numpy as np
-from scipy.interpolate import InterpolatedUnivariateSpline, interp1d
-import copy
-import csv
 import re
-from collections import namedtuple
+from textwrap import dedent
+from contextlib import redirect_stdout
+from freeqdsk import peqdsk
 
 def ion_species_selector(nucleons, charge):
     """
@@ -27,21 +26,21 @@ def ion_species_selector(nucleons, charge):
     Might need to update with more specific masses, such as 6.94 for Li-7, etc.
     """
     if nucleons == 1:
-        if charge == 1:
+        if charge.m == 1:
             return "hydrogen"
         else:
             print(
                 "You have a species with a single nucleon which is not a proton. Strange. Returning neutron for now. \n"
             )
             return "neutron"
-    if nucleons == 2 and charge == 1:
+    if nucleons == 2 and charge.m == 1:
         return "deuterium"
-    elif nucleons == 4 and charge == 2:
+    elif nucleons == 4 and charge.m == 2:
         return "helium"
     elif nucleons == 3:
-        if charge == 1:
+        if charge.m == 1:
             return "tritium"
-        if charge == 2:
+        if charge.m == 2:
             return "helium3"
     else:
         return "impurity"
@@ -52,7 +51,7 @@ def np_to_T(n, p):
     n is in m^{-3}, T is in eV, p is in Pascals.
     Returns temperature in eV.
     """
-    return np.divide(p, n) / ((1.381e-23) * 11600)
+    return np.divide(p, n).to("eV")
 
 
 class KineticsReaderpFile(KineticsReader):
@@ -60,8 +59,6 @@ class KineticsReaderpFile(KineticsReader):
         self,
         filename: PathLike,
         eq_file: PathLike = None,
-        time_index: int = -1,
-        time: Optional[float] = None,
     ) -> Dict[str, Species]:
         """
         Reads in Osborne pFile. Your pFile should just be called, pFile.
@@ -79,153 +76,156 @@ class KineticsReaderpFile(KineticsReader):
             )
 
         # Read pFile, get generic data.
-        pFile = PFileReader(str(filename))
-        psi_n = pFile.__getattribute__("ne").x
-        electron_temp_data = (
-            pFile.__getattribute__("te").y * 1000
-        )  # Converting keV to eV.
-        electron_dens_data = (
-            pFile.__getattribute__("ne").y * 1e20
-        )  # Converting 10^20 m^-3 to m^-3.
+        with redirect_stdout(None), open(filename) as f:
+            data = peqdsk.read(f)
 
-        electron_temp_func = InterpolatedUnivariateSpline(
-            psi_n, electron_temp_data
-        )  # Interpolate on psi_n.
-        electron_dens_func = InterpolatedUnivariateSpline(psi_n, electron_dens_data)
+        profiles = data["profiles"]
+        species = data["species"]
+
+        # Interpolate on psi_n.
+        te_psi_n = profiles["te"]["psinorm"] * units.dimensionless
+        electron_temp_data = profiles["te"]["data"] * 1e3 * units.eV
+        electron_temp_func = UnitSpline(te_psi_n, electron_temp_data)
+
+        ne_psi_n = profiles["ne"]["psinorm"] * units.dimensionless
+        electron_dens_data = profiles["ne"]["data"] * 1e20 * units.meter**-3
+        electron_dens_func = UnitSpline(ne_psi_n, electron_dens_data)
 
         # Read geqdsk file, obtain rho_func.
         geqdsk_equilibrium = read_equilibrium(str(eq_file))
-        rho_g = geqdsk_equilibrium["r_minor"].values
-        psi_n_g = geqdsk_equilibrium["psi_n"].values
-        rho_func = InterpolatedUnivariateSpline(psi_n_g, rho_g)
+        rho_g = geqdsk_equilibrium["r_minor"].values * units.lref_minor_radius
+        psi_n_g = geqdsk_equilibrium["psi_n"].values * units.dimensionless
+        rho_func = UnitSpline(psi_n_g, rho_g)
 
-        if "omeg" in pFile._params:
-            omega_data = pFile.__getattribute__("omeg").y
+        if "omeg" in profiles.keys():
+            omega_psi_n = profiles["omeg"]["psinorm"] * units.dimensionless
+            omega_data = profiles["omeg"]["data"] * 1e3 * units.radians / units.second
         else:
-            omega_data = np.zeros(len(psi_n), dtype="float")
+            omega_psi_n = te_psi_n * units.dimensionless
+            omega_data = np.zeros(len(omega_psi_n), dtype="float") * units.radians / units.second
 
-        omega_func = InterpolatedUnivariateSpline(psi_n, omega_data)
+        omega_func = UnitSpline(omega_psi_n, omega_data)
+
+        if "vtor1" in profiles.keys():
+            rot_psi_n = profiles["vtor1"]["psinorm"] * units.dimensionless
+            rotation_data = profiles["vtor1"]["data"] * 1e3 * units.meter / units.second
+        else:
+            rot_psi_n = te_psi_n * units.dimensionless
+            rotation_data = np.zeros(len(rot_psi_n), dtype="float") * units.meter / units.second
+
+        rotation_func = UnitSpline(rot_psi_n, rotation_data)
 
         electron = Species(
             species_type="electron",
-            charge=-1,
+            charge=-1*units.elementary_charge,
             mass=electron_mass,
             dens=electron_dens_func,
             temp=electron_temp_func,
             ang=omega_func,
+            rot=rotation_func,
             rho=rho_func,
         )
 
         result = {"electron": electron}
-
-        num_ions = pFile.__getattribute__("ions").nions
+        num_ions = len(species)
 
         # Check whether fast particles.
-        fast_particle = 0
-        if "nb" in pFile._params:
-            fast_particle = 1
-            print("Fast particles present in pFile \n.")
+        try:
+            if np.all(profiles["nb"]["data"] == 0.0):
+                fast_particle = 0
+            else:
+                fast_particle = 1
+        except KeyError:
+            fast_particle = 0
 
         num_thermal_ions = num_ions - fast_particle
 
         # thermal ions have same temperature in pFile.
-        ion_temp_data = pFile.__getattribute__("ti").y * 1000  # Converting keV to eV.
-        ion_temp_func = InterpolatedUnivariateSpline(
-            psi_n, ion_temp_data
-        )  # Interpolate on psi_n.
+        ti_psi_n = profiles["ti"]["psinorm"] * units.dimensionless
+        ion_temp_data = profiles["ti"]["data"] * 1e3 * units.eV
+        ion_temp_func = UnitSpline(ti_psi_n, ion_temp_data)
 
         for ion_it in np.arange(num_thermal_ions):
-            """
-            --Ordering of ['N Z A'] in pFile is:
+            if ion_it == num_thermal_ions - 1:
+                ni_psi_n = profiles["ni"]["psinorm"] * units.dimensionless
+                ion_dens_data = profiles["ni"]["data"] * 1e20 * units.meter ** -3
+                ion_dens_func = UnitSpline(ni_psi_n, ion_dens_data)
 
-            N Z A:
-            impurities
-            main ion
-            fast ion
+                ion_charge = species[ion_it]["Z"] * units.elementary_charge
+                ion_nucleons = species[ion_it]["A"]
+                ion_mass = ion_nucleons * hydrogen_mass
 
-            --All thermal ions have the same temperature.
-            """
-
-            if ion_it == num_thermal_ions - 1:  # Main ion.
-                ion_dens_data = (
-                    pFile.__getattribute__("ni").y * 1e20
-                )  # Converting 10^20 m^-3 to m^-3.
-
-                charge = pFile.__getattribute__("ions").Z[ion_it]
-                nucleons = pFile.__getattribute__("ions").A[ion_it]
-                mass = nucleons * hydrogen_mass
-
-                species_name = ion_species_selector(nucleons, charge)
-                ion_dens_func = InterpolatedUnivariateSpline(psi_n, ion_dens_data)
+                species_name = ion_species_selector(ion_nucleons, ion_charge)
 
                 result[species_name] = Species(
                     species_type=species_name,
-                    charge=charge,
-                    mass=mass,
+                    charge=ion_charge,
+                    mass=ion_mass,
                     dens=ion_dens_func,
                     temp=ion_temp_func,
                     ang=omega_func,
+                    rot=rotation_func,
                     rho=rho_func,
                 )
 
-            else:  # Impurities.
-                ion_dens_data = (
-                    pFile.__getattribute__("nz{}".format(int(ion_it + 1))).y * 1e20
-                )  # Converting 10^20 m^-3 to m^-3.
+            else:
+                try:
+                    nz_psi_n = profiles[f"nz{ion_it+1}"]["psinorm"] * units.dimensionless
+                    impurity_dens_data = profiles[f"nz{ion_it+1}"]["data"] * 1e20 * units.meter ** -3
+                except KeyError:
+                    nz_psi_n = ni_psi_n
+                    impurity_dens_data = ion_dens_data * 0.0
 
-                charge = pFile.__getattribute__("ions").Z[ion_it]
-                nucleons = pFile.__getattribute__("ions").A[ion_it]
-                mass = nucleons * hydrogen_mass
+                impurity_dens_func = UnitSpline(nz_psi_n, impurity_dens_data)
 
-                species_name = ion_species_selector(nucleons, charge)
-                ion_dens_func = InterpolatedUnivariateSpline(psi_n, ion_dens_data)
+                impurity_charge = species[ion_it]["Z"] * units.elementary_charge
+                impurity_nucleons = species[ion_it]["A"]
+                impurity_mass = impurity_nucleons * hydrogen_mass
 
+                species_name = ion_species_selector(impurity_nucleons, impurity_charge)
                 result[species_name] = Species(
                     species_type=species_name,
-                    charge=charge,
-                    mass=mass,
-                    dens=ion_dens_func,
+                    charge=impurity_charge,
+                    mass=impurity_mass,
+                    dens=impurity_dens_func,
                     temp=ion_temp_func,
                     ang=omega_func,
+                    rot=rotation_func,
                     rho=rho_func,
                 )
 
-        if fast_particle:  # Adding the fast particle species.
-            fast_ion_dens_data = (
-                pFile.__getattribute__("nb").y * 1e20
-            )  # Converting 10^20 m^-3 to m^-3.
-            fast_ion_press_data = pFile.__getattribute__("pb").y
+        if fast_particle == 1:  # Adding the fast particle species.
+            nb_psi_n = profiles["nb"]["psinorm"] * units.dimensionless
+            fast_ion_dens_data = profiles["nb"]["data"] * 1e20 * units.meter ** -3
 
-            proceed = 1
-            if np.sum(fast_ion_dens_data) < 0.01:
-                print("Fast ion density empty. Not reading fast ions.")
-                proceed = 0
-            if proceed == 1:
-                # estimate fast particle temperature from pressure and density. Very approximate.
-                fast_ion_temp_data = np_to_T(fast_ion_dens_data, fast_ion_press_data)
+            pb_psi_n = profiles["pb"]["psinorm"] * units.dimensionless
+            fast_ion_press_data = profiles["pb"]["data"] * 1e3 * units.pascals
 
-                fast_ion_dens_func = InterpolatedUnivariateSpline(
-                    psi_n, fast_ion_dens_data
-                )
-                fast_ion_temp_func = InterpolatedUnivariateSpline(
-                    psi_n, fast_ion_temp_data
-                )
+            if np.all(pb_psi_n != nb_psi_n):
+                fast_ion_press_func = UnitSpline(pb_psi_n, fast_ion_press_data)
+                fast_ion_press_data = fast_ion_press_func(nb_psi_n)
 
-                charge = pFile.__getattribute__("ions").Z[-1]
-                nucleons = pFile.__getattribute__("ions").A[-1]
-                mass = nucleons * hydrogen_mass
+            fast_ion_temp_data = np_to_T(fast_ion_dens_data, fast_ion_press_data)
 
-                fast_species = ion_species_selector(nucleons, charge) + str("_fast")
+            fast_ion_dens_func = UnitSpline(nb_psi_n, fast_ion_dens_data)
+            fast_ion_temp_func = UnitSpline(nb_psi_n, fast_ion_temp_data)
 
-                result[fast_species] = Species(
-                    species_type=fast_species,
-                    charge=charge,
-                    mass=mass,
-                    dens=fast_ion_dens_func,
-                    temp=fast_ion_temp_func,
-                    ang=omega_func,
-                    rho=rho_func,
-                )
+            fast_ion_charge = species[-1]["Z"] * units.elementary_charge
+            fast_ion_nucleons = species[-1]["A"]
+            fast_ion_mass = ion_nucleons * hydrogen_mass
+
+            fast_species = ion_species_selector(fast_ion_nucleons, fast_ion_charge) + str("_fast")
+
+            result[fast_species] = Species(
+                species_type=fast_species,
+                charge=fast_ion_charge,
+                mass=fast_ion_mass,
+                dens=fast_ion_dens_func,
+                temp=fast_ion_temp_func,
+                ang=omega_func,
+                rot=rotation_func,
+                rho=rho_func,
+            )
 
         return result
 
