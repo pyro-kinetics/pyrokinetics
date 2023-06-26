e0d54470f4839ed99f8ed6f13092f9b121502fde
Peter Hill <peter.hill@york.ac.uk>
2023-06-26 11:57:59 +0100

    Docs: Switch to sphinx-book-theme

--- docs/conf.py
+++ docs/conf.py
@@ -63,12 +63,22 @@ autodoc_type_aliases = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ["_static"]
 html_static_path = []
+
+html_theme_options = {
+    "repository_url": "https://github.com/pyro-kinetics/pyrokinetics",
+    "repository_branch": "unstable",
+    "path_to_docs": "docs",
+    "use_edit_page_button": True,
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "home_page_in_toc": False,
+}
 
 pygments_style = "sphinx"
