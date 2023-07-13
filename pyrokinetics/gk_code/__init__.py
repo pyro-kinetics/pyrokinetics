6e16d90366384293bd59d80f814dfb55d5d487e0
Peter Hill <peter.hill@york.ac.uk>
2023-07-13 16:32:33 +0100

    Merge GK output and input reader modules

--- pyrokinetics/gk_code/__init__.py
+++ pyrokinetics/gk_code/__init__.py
@@ -1,18 +1,13 @@
+from .cgyro import GKInputCGYRO, GKOutputReaderCGYRO  # noqa
+from .gene import GKInputGENE, GKOutputReaderGENE  # noqa
+from .gs2 import GKInputGS2, GKOutputReaderGS2  # noqa
+from .tglf import GKInputTGLF, GKOutputReaderTGLF  # noqa
 from .gk_input import GKInput, gk_inputs
-from .gs2 import GKInputGS2
-from .cgyro import GKInputCGYRO
-from .gene import GKInputGENE
-from .tglf import GKInputTGLF
+from .gk_output import GKOutput, supported_gk_output_types  # noqa
 
 gk_inputs["GS2"] = GKInputGS2
 gk_inputs["CGYRO"] = GKInputCGYRO
 gk_inputs["GENE"] = GKInputGENE
 gk_inputs["TGLF"] = GKInputTGLF
-
-from .gk_output import GKOutput, supported_gk_output_types  # noqa
-from .GKOutputReaderGS2 import GKOutputReaderGS2  # noqa
-from .GKOutputReaderCGYRO import GKOutputReaderCGYRO  # noqa
-from .GKOutputReaderGENE import GKOutputReaderGENE  # noqa
-from .GKOutputReaderTGLF import GKOutputReaderTGLF  # noqa
 
 __all__ = ["GKInput", "gk_inputs", "GKOutput", "supported_gk_output_types"]
