148638983d7655c292ce5028850d213796414cc3
Bhavin Patel <15210802+bpatel2107@users.noreply.github.com>
2023-07-26 15:25:38 +0100

    Add units to gamma_exb when reading and tests

--- pyrokinetics/gk_code/GKInputTGLF.py
+++ pyrokinetics/gk_code/GKInputTGLF.py
@@ -353,7 +353,7 @@ class GKInputTGLF(GKInput):
             self.data["betae"] * ureg.beta_ref_ee_Bunit * ne_norm * Te_norm
         )
 
-        numerics_data["gamma_exb"] = self.data.get("vexb_shear", 0.0)
+        numerics_data["gamma_exb"] = self.data.get("vexb_shear", 0.0) * ureg.vref_nrl / ureg.lref_minor_radius
 
         return Numerics(**numerics_data)
 
