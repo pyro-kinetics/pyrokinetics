de47359a258a63e777f04168b697d3eb737f0457
Liam Pattinson <liampattinson@gmail.com>
2023-07-26 15:51:49 +0100

    Prevent importing databases on Python <3.9

--- pyrokinetics/databases/__init__.py
+++ pyrokinetics/databases/__init__.py
@@ -1,4 +1,8 @@
-from .imas import pyro_to_ids, ids_to_pyro
+from platform import python_version_tuple
 
+__all__ = []
 
-__all__ = ["pyro_to_ids", "ids_to_pyro"]
+if tuple(int(x) for x in python_version_tuple()[:2]) >= (3, 9):
+    from .imas import pyro_to_ids, ids_to_pyro
+
+    __all__.extend(["pyro_to_ids", "ids_to_pyro"])
