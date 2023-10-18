158a0db5426ed797dc12c51d68f01cee60f65a8a
Bhavin Patel <15210802+bpatel2107@users.noreply.github.com>
2023-10-18 19:40:08 +0100

    Set up GACODE kinetics/equilibrium

--- src/pyrokinetics/equilibrium/__init__.py
+++ src/pyrokinetics/equilibrium/__init__.py
@@ -9,6 +9,7 @@ from .flux_surface import FluxSurface
 # Import each built-in reader to register them with Equilibrium
 from . import geqdsk  # noqa
 from . import transp  # noqa
+from . import gacode  # noqa
 
 # Register external plugins with Equilibrium
 from ..plugins import register_file_reader_plugins
