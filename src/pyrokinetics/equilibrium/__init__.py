c826a01980bb2a40c3d8d3d864b8354aeecaadea
Liam Pattinson <liampattinson@gmail.com>
2023-11-03 17:36:14 +0000

    Reorder imports for equilibrium and kinetics

--- src/pyrokinetics/equilibrium/__init__.py
+++ src/pyrokinetics/equilibrium/__init__.py
@@ -1,7 +1,5 @@
-from ..plugins import register_file_reader_plugins
-from . import gacode  # noqa
-from . import geqdsk  # noqa
-from . import transp  # noqa
+# isort: skip_file
+
 from .equilibrium import (
     Equilibrium,
     EquilibriumCOCOSWarning,
@@ -9,6 +7,13 @@ from .equilibrium import (
     supported_equilibrium_types,
 )
 from .flux_surface import FluxSurface
+
+# Import each module to register the associated readers
+from . import geqdsk  # noqa
+from . import transp  # noqa
+from . import gacode  # noqa
+
+from ..plugins import register_file_reader_plugins
 
 register_file_reader_plugins("equilibrium", Equilibrium)
 
--- src/pyrokinetics/kinetics/__init__.py
+++ src/pyrokinetics/kinetics/__init__.py
@@ -1,10 +1,15 @@
-from ..plugins import register_file_reader_plugins
-from .gacode import KineticsReaderGACODE
-from .jetto import KineticsReaderJETTO  # noqa
+# isort: skip_file
+
 from .kinetics import Kinetics, read_kinetics, supported_kinetics_types
-from .pfile import KineticsReaderpFile  # noqa
+
+# Import each module to register the associated readers
 from .scene import KineticsReaderSCENE  # noqa
 from .transp import KineticsReaderTRANSP  # noqa
+from .pfile import KineticsReaderpFile  # noqa
+from .jetto import KineticsReaderJETTO  # noqa
+from .gacode import KineticsReaderGACODE  # noqa
+
+from ..plugins import register_file_reader_plugins
 
 register_file_reader_plugins("kinetics", Kinetics)
 
