ae421b6686a1538fff5091a94162db09dc99fbda
Bhavin Patel <15210802+bpatel2107@users.noreply.github.com>
2023-11-29 18:40:33 +0000

    Add ideal_ball solver and example

---
+++ docs/examples/example_ideal_ballooning.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pyrokinetics import Pyro
+from pyrokinetics.diagnostics import Diagnostics
+from pyrokinetics import template_dir
+
+nshat = 15
+nbprime = 20
+shat = np.linspace(0.0, 2, nshat)
+bprime = np.linspace(0.0, -0.5, nbprime)
+
+pyro = Pyro(gk_file=template_dir / "input.gs2")
+
+gamma = np.empty((nshat, nbprime))
+
+for i_s, s in enumerate(shat):
+    for i_b, b in enumerate(bprime):
+        pyro.local_geometry.shat = s
+        pyro.local_geometry.beta_prime = b
+        diag = Diagnostics(pyro)
+        gamma[i_s, i_b] = diag.ideal_ballooning_solver()
+
+
+fig, ax = plt.subplots(1, 1, sharex=True, figsize=(8, 7))
+
+cs = ax.contourf(abs(bprime), shat, gamma)
+cs_0 = ax.contour(
+    abs(bprime),
+    shat,
+    gamma,
+    colors="w",
+    linewidths=(2,),
+    ls="--",
+    levels=[
+        0.0,
+    ],
+    label="PYRO",
+)
+
+ax.clabel(cs_0, fmt="%.1e", colors="w", fontsize=22)
+ax.set_ylabel(r"$\hat{s}$")
+ax.set_xlabel(r"$|\beta'|$")
+fig.colorbar(cs)
+ax.set_title("IBM growth rate")
+plt.show()
--- src/pyrokinetics/diagnostics/__init__.py
+++ src/pyrokinetics/diagnostics/__init__.py
@@ -1,6 +1,8 @@
 import numpy as np
 import xrft
 from scipy.interpolate import RectBivariateSpline
+from scipy.sparse.linalg import eigs
+from scipy.integrate import simps
 
 from ..pyro import Pyro
 
@@ -21,11 +23,6 @@ class Diagnostics:
     """
 
     def __init__(self, pyro: Pyro):
-        if pyro.gk_output is None:
-            raise RuntimeError(
-                "Diagnostics: Please load gk output files (Pyro.load_gk_output)"
-                " before using any diagnostic"
-            )
         self.pyro = pyro
 
     def poincare(
@@ -75,6 +72,12 @@ class Diagnostics:
         RuntimeError: in case of linear simulation
         """
         import pint_xarray  # noqa
+
+        if self.pyro.gk_output is None:
+            raise RuntimeError(
+                "Diagnostics: Please load gk output files (Pyro.load_gk_output)"
+                " before using any diagnostic"
+            )
 
         if self.pyro.gk_code not in ["CGYRO", "GS2", "GENE"]:
             raise NotImplementedError(
@@ -248,3 +251,232 @@ class Diagnostics:
             )
         )
         return np.real(value)
+
+    def gs2_geometry_terms(self, ntheta_multiplier: int = 10):
+        nperiod = self.pyro.numerics.nperiod
+        ntheta = self.pyro.numerics.ntheta * ntheta_multiplier
+
+        theta_max = ((2 * nperiod) - 1) * np.pi
+        theta_even = np.linspace(-theta_max, theta_max, ntheta)
+
+        self.pyro.load_metric_terms(theta=theta_even)
+
+        metric = self.pyro.metric_terms
+
+        theta = metric.regulartheta
+
+        dpdrho = metric.mu0dPdr
+        dpsidrho = metric.dpsidr
+
+        # Metric tensor terms
+        g_ra_contr = metric.field_aligned_contravariant_metric("r", "alpha")
+        g_ra = metric.field_aligned_contravariant_metric("r", "alpha")
+        g_aa = metric.field_aligned_contravariant_metric("alpha", "alpha")
+
+        g_at = metric.field_aligned_covariant_metric("alpha", "theta")
+
+        grad_alpha = np.sqrt(g_aa)
+
+        g_tt = metric.field_aligned_covariant_metric("theta", "theta")
+        dg_tt_dr = metric.dg_theta_theta_dr
+        dg_tt_dtheta = metric.dg_theta_theta_dtheta
+
+        g_rt = metric.field_aligned_covariant_metric("r", "theta")
+        g_rr = metric.field_aligned_contravariant_metric("r", "r")
+        g_rr_tor = metric.toroidal_contravariant_metric("r", "r")
+
+        dg_zz_drho = metric.dg_zeta_zeta_dr
+        dg_zz_dtheta = metric.dg_zeta_zeta_dtheta
+
+        g_tt_tor = metric.toroidal_covariant_metric("theta", "theta")
+        g_zz_tor = metric.toroidal_covariant_metric("zeta", "zeta")
+
+        # Jacobian and derivatives
+        jacob = metric.Jacobian
+        djacob_drho = metric.dJacobian_dr
+        djacob_dtheta = metric.dJacobian_dtheta
+
+        # R and derivatives
+        R = metric.R
+        dR_dr = metric.dRdr
+        dR_dtheta = metric.dRdtheta
+        d2R_dtheta2 = metric.d2Rdtheta2
+
+        # Z and derivatives
+        Z = metric.Z
+        dZ_dr = metric.dZdr
+        dZ_dtheta = metric.dZdtheta
+        d2Z_dtheta2 = metric.d2Zdtheta2
+
+        # B_theta and derivatives
+        B_theta = dpsidrho / jacob
+        dB_theta_drho = metric.dB_theta_drho
+        dB_theta_dtheta = metric.dB_theta_dtheta
+
+        # B_zeta and derivatives
+        B_zeta = metric.B_zeta
+        dB_zeta_dr = metric.dB_zeta_dr
+
+        # B_mag and derivatives
+        B_mag = metric.B_magnitude
+        dB_dr = metric.dB_magnitude_dr
+        dB_dtheta = metric.dB_magnitude_dtheta
+
+        # Drifts
+        gbdrift = -2 * (dB_dr - g_rt / g_tt * dB_dtheta) / B_mag
+        press = -2 * dpdrho * (jacob / np.sqrt(g_tt)) / B_mag / dpsidrho
+        gbdrift0 = 2 * metric.dqdr * g_at / g_tt * dB_dtheta / B_mag
+        cvdrift = gbdrift + press
+
+        # GDS values
+        gds2 = (grad_alpha * dpsidrho) ** 2
+        gds21 = -(dpsidrho**2) * g_ra_contr * metric.dqdr
+        gds22 = (dpsidrho * metric.dqdr) ** 2 * g_rr
+
+        # Parallel gradient
+        gradpar = 1 / np.sqrt(g_tt)
+
+        # B field
+        bmag = B_mag
+        bpol = dpsidrho * np.sqrt(g_rr) / R
+
+        # Grad rho
+        grho = np.sqrt(g_rr)
+
+        # Eikonal
+        S = -metric.alpha
+        dS_dr = -metric.dalpha_dr * dpsidrho * grho
+        # dS_dr = - g_ra * g_zz_tor / dpsidrho * g_rr # / bmag * np.sqrt(grho)
+        # dS_dr = - metric.dalpha_dr * metric.rho / metric.q  # * grho
+
+        return (
+            dpdrho,
+            theta,
+            bmag,
+            bpol,
+            gradpar,
+            cvdrift,
+            gds2,
+            gbdrift,
+            gbdrift0,
+            grho,
+            gds21,
+            gds22,
+            R,
+            Z,
+            dR_dr,
+            dZ_dr,
+            S,
+            dS_dr,
+        )
+
+    def ideal_ballooning_solver(self):
+        r"""
+        Adapted from ideal-ballooning-solver
+        https://github.com/rahulgaur104/ideal-ballooning-solver/blob/master/ball_scan.py
+
+        """
+
+        (
+            dpdrho,
+            theta,
+            bmag,
+            bpol,
+            gradpar,
+            cvdrift,
+            gds2,
+            gbdrift,
+            gbdrift0,
+            grho,
+            gds21,
+            gds22,
+            R,
+            Z,
+            dR_dr,
+            dZ_dr,
+            S,
+            dS_dr,
+        ) = self.gs2_geometry_terms()
+
+        gamma, X_arr, dX_arr, g_arr, c_arr, f_arr = gamma_ball_full(
+            dpdrho, theta, bmag, gradpar, cvdrift, gds2
+        )
+
+        return gamma
+
+
+def gamma_ball_full(
+    dPdrho, theta_PEST, B, gradpar, cvdrift, gds2, vguess=None, sigma0=0.42
+):
+    # Inputs  : geometric coefficients(normalized with a_N, and B_N)
+    #           on an equispaced theta_PEST grid
+    # Outputs : maximum ballooning growth rate gamma
+    theta_ball = theta_PEST
+    ntheta = len(theta_ball)
+
+    # Note that gds2 is (dpsidrho*|grad alpha|/(a_N*B_N))**2.
+    g = np.abs(gradpar) * gds2 / (B)
+    c = -1 * dPdrho * cvdrift * 1 / (np.abs(gradpar) * B)
+    f = gds2 / B**2 * 1 / (np.abs(gradpar) * B)
+
+    len1 = len(g)
+
+    ##Uniform half theta ball
+    theta_ball_u = np.linspace(theta_ball[0], theta_ball[-1], len1)
+
+    g_u = np.interp(theta_ball_u, theta_ball, g)
+    c_u = np.interp(theta_ball_u, theta_ball, c)
+    f_u = np.interp(theta_ball_u, theta_ball, f)
+
+    # uniform theta_ball on half points with half the size, i.e., only from [0, (2*nperiod-1)*np.pi]
+    theta_ball_u_half = (theta_ball_u[:-1] + theta_ball_u[1:]) / 2
+    h = np.diff(theta_ball_u_half)[2]
+    g_u_half = np.interp(theta_ball_u_half, theta_ball, g)
+    g_u1 = g_u[:]
+    c_u1 = c_u[:]
+    f_u1 = f_u[:]
+
+    len2 = int(len1) - 2
+    A = np.zeros((len2, len2))
+
+    A = (
+        np.diag(g_u_half[1:-1] / f_u1[2:-1] * 1 / h**2, -1)
+        + np.diag(
+            -(g_u_half[1:] + g_u_half[:-1]) / f_u1[1:-1] * 1 / h**2
+            + c_u1[1:-1] / f_u1[1:-1],
+            0,
+        )
+        + np.diag(g_u_half[1:-1] / f_u1[1:-2] * 1 / h**2, 1)
+    )
+
+    # Method without M is approx 3 X faster with Arnoldi iteration
+    # Perhaps, we should try dstemr as suggested by Max Ruth. However, I doubt if
+    # that will give us a significant speedup
+    w, v = eigs(A, 1, sigma=sigma0, v0=vguess, tol=5.0e-7, OPpart="r")
+    # w, v  = eigs(A, 1, sigma=1.0, tol=1E-6, OPpart='r')
+    # w, v  = eigs(A, 1, sigma=1.0, tol=1E-6, OPpart='r')
+
+    ### Richardson extrapolation
+    X = np.zeros((len2 + 2,))
+    dX = np.zeros((len2 + 2,))
+    # X[1:-1]     = np.reshape(v[:, idx_max].real, (-1,))/np.max(np.abs(v[:, idx_max].real))
+    X[1:-1] = np.reshape(v[:, 0].real, (-1,)) / np.max(np.abs(v[:, 0].real))
+
+    X[0] = 0.0
+    X[-1] = 0.0
+
+    dX[0] = (-1.5 * X[0] + 2 * X[1] - 0.5 * X[2]) / h
+    dX[1] = (X[2] - X[0]) / (2 * h)
+
+    dX[-2] = (X[-1] - X[-3]) / (2 * h)
+    dX[-1] = (0.5 * X[-3] - 2 * X[-2] + 1.5 * 0.0) / (h)
+
+    dX[2:-2] = 2 / (3 * h) * (X[3:-1] - X[1:-3]) - (X[4:] - X[0:-4]) / (12 * h)
+
+    Y0 = -g_u1 * dX**2 + c_u1 * X**2
+    Y1 = f_u1 * X**2
+    # plt.plot(range(len3+2), X, range(len3+2), dX); plt.show()
+    gam = simps(Y0) / simps(Y1)
+
+    # return np.sign(gam)*np.sqrt(abs(gam)), X, dX, g_u1, c_u1, f_u1
+    return gam, X, dX, g_u1, c_u1, f_u1
