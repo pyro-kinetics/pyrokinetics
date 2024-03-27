a83138ff736fb3a7b138cee3074ffa0e1f9ee82c
Michael Hardman <michael.hardman@tokamakenergy.co.uk>
2024-03-27 09:55:38 +0000

    Add stella to normalisations object

--- src/pyrokinetics/normalisation.py
+++ src/pyrokinetics/normalisation.py
@@ -246,6 +246,7 @@ NORMALISATION_CONVENTIONS = {
     "pyrokinetics": Convention("pyrokinetics"),
     "cgyro": Convention("cgyro", bref=ureg.bref_Bunit, rhoref=ureg.rhoref_unit),
     "gs2": Convention("gs2", vref=ureg.vref_most_probable, rhoref=ureg.rhoref_gs2),
+    "stella": Convention("stella", vref=ureg.vref_most_probable, rhoref=ureg.rhoref_gs2),
     "gene": Convention("gene", lref=ureg.lref_major_radius, rhoref=ureg.rhoref_pyro),
     "imas": Convention(
         "imas",
