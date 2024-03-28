3817fb998a8c6ff0ee9e0acc3bd2e467e238218a
Liam Pattinson <liampattinson@gmail.com>
2024-03-28 18:12:56 +0000

    Formatting fix for example_merge_species

--- docs/examples/example_merge_species.py
+++ docs/examples/example_merge_species.py
@@ -21,9 +21,12 @@ def main():
     pyro.load_local(psi_n=0.5, local_geometry="Miller")
 
     # merge species 'impurity1' into 'deuterium'
-    pyro.local_species.merge_species('deuterium', ['impurity1'],
-            keep_base_species_mass=False,
-            keep_base_species_z=False)
+    pyro.local_species.merge_species(
+        "deuterium",
+        ["impurity1"],
+        keep_base_species_mass=False,
+        keep_base_species_z=False,
+    )
 
     # write to file
     pyro.write_gk_file(file_name="input.gene", gk_code="GENE")
