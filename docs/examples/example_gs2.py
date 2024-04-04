469d0ff30ed4079c66f3e1c6ddc1f51da26a59ae
Arkaprava Bokshi <arka.bokshi@york.ac.uk>
2024-04-04 14:44:05 +0100

    remove duplicate examples

--- docs/examples/example_gs2.py
+++ docs/examples/example_gs2.py
@@ -13,9 +13,3 @@ flags = {
 
 pyro.add_flags(flags)
 pyro.write_gk_file(file_name="step.gs2")
-
-pyro.gk_code = "CGYRO"
-pyro.write_gk_file(file_name="step.cgyro")
-
-pyro.gk_code = "GENE"
-pyro.write_gk_file(file_name="step.gene")
