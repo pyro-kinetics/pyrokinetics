Added test for non-standard normalisation in GS2 and use isclose when checking for normalising species. Fixed other smaller bugs

src/pyrokinetics/gk_code/cgyro.py
@@ -575,9 +575,9 @@ class GKInputCGYRO(GKInput, FileReader, file_type="CGYRO", reads=GKInput):
             electron_index = "AE"
             found_electron = True
 
-            if self.data[f"DENS_AE"] == 1.0:
+            if np.isclose(self.data[f"DENS_AE"], 1.0):
                 dens_index.append("AE")
-            if self.data[f"TEMP_AE"] == 1.0:
+            if np.isclose(self.data[f"TEMP_AE"], 1.0):
                 temp_index.append("AE")
         else:
             for i_sp in range(self.data["N_SPECIES"]):
@@ -588,9 +588,9 @@ class GKInputCGYRO(GKInput, FileReader, file_type="CGYRO", reads=GKInput):
                     electron_index = i_sp + 1
                     found_electron = True
 
-                if self.data[f"DENS_{i_sp+1}"] == 1.0:
+                if np.isclose(self.data[f"DENS_{i_sp+1}"], 1.0):
                     dens_index.append(i_sp+1)
-                if self.data[f"TEMP_{i_sp+1}"] == 1.0:
+                if np.isclose(self.data[f"TEMP_{i_sp+1}"], 1.0):
                     temp_index.append(i_sp+1)
 
         if not found_electron:
@@ -609,7 +609,7 @@ class GKInputCGYRO(GKInput, FileReader, file_type="CGYRO", reads=GKInput):
         me_md = (electron_mass / deuterium_mass).m
         me_mh = (electron_mass / hydrogen_mass).m
 
-        if e_mass == 1.0:
+        if np.isclose(e_mass, 1.0):
             references["mref_species"] = "electron"
         elif np.isclose(e_mass, me_md, rtol=0.1):
             references["mref_species"] = "deuterium"
@@ -622,7 +622,7 @@ class GKInputCGYRO(GKInput, FileReader, file_type="CGYRO", reads=GKInput):
             references["nref_species"] = "electron"
         else:
             for i_sp in dens_index:
-                if self.data[f"MASS{i_sp}"] == 1.0:
+                if np.isclose(self.data[f"MASS{i_sp}"], 1.0):
                     references["nref_species"] = references["mref_species"]
 
         if references["nref_species"] is None:
@@ -632,7 +632,7 @@ class GKInputCGYRO(GKInput, FileReader, file_type="CGYRO", reads=GKInput):
             references["tref_species"] = "electron"
         else:
             for i_sp in temp_index:
-                if self.data[f"TEMP_{i_sp}"] == 1.0:
+                if np.isclose(self.data[f"TEMP_{i_sp}"], 1.0):
                     references["tref_species"] = references["mref_species"]
 
         if references["nref_species"] is None:
@@ -648,6 +648,7 @@ class GKInputCGYRO(GKInput, FileReader, file_type="CGYRO", reads=GKInput):
         if references == default_references:
             return {}
         else:
+            self.norm_convention = f"{self.code_name.lower()}_bespoke"
             return references
 
     def set(
@@ -1037,10 +1038,8 @@ class GKOutputReaderCGYRO(FileReader, file_type="CGYRO", reads=GKOutput):
         input_str = raw_data["input"]
         gk_input = GKInputCGYRO()
         gk_input.read_str(input_str)
-        #TODO Need better way to get bespoke norm
-        norm_dict = gk_input._get_normalisation()
-        if norm_dict:
-            gk_input.norm_convention = f"{gk_input.code_name.lower()}_bespoke"
+        gk_input._get_normalisation()
+
         return raw_data, gk_input, input_str
 
     @staticmethod