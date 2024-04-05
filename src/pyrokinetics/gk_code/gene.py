Added test for non-standard normalisation in GS2 and use isclose when checking for normalising species. Fixed other smaller bugs

src/pyrokinetics/gk_code/gene.py
@@ -562,6 +562,16 @@ class GKInputGENE(GKInput, FileReader, file_type="GENE", reads=GKInput):
                       "vref": "nrl"
                       }
 
+        pyro_default_references = {"nref_species": "electron",
+                      "tref_species": "electron",
+                      "mref_species": "deuterium",
+                      "bref": "B0",
+                      "lref": "minor_radius",
+                      "ne": 1.0,
+                      "te": 1.0,
+                      "rgeo_rmaj": 1.0,
+                      "vref": "nrl"
+                      }
         references = copy.copy(default_references)
 
         dens_index = []
@@ -580,9 +590,9 @@ class GKInputGENE(GKInput, FileReader, file_type="GENE", reads=GKInput):
                 found_electron = True
 
             # Find all reference values
-            if self.data["species"][i_sp]["dens"] == 1.0:
+            if np.isclose(self.data["species"][i_sp]["dens"], 1.0):
                 dens_index.append(i_sp)
-            if self.data["species"][i_sp]["temp"] == 1.0:
+            if np.isclose(self.data["species"][i_sp]["temp"], 1.0):
                 temp_index.append(i_sp)
 
         if not found_electron:
@@ -602,7 +612,7 @@ class GKInputGENE(GKInput, FileReader, file_type="GENE", reads=GKInput):
         me_md = (electron_mass / deuterium_mass).m
         me_mh = (electron_mass / hydrogen_mass).m
 
-        if e_mass == 1.0:
+        if np.isclose(e_mass, 1.0):
             references["mref_species"] = "electron"
         elif np.isclose(e_mass, me_md, rtol=0.1):
             references["mref_species"] = "deuterium"
@@ -615,7 +625,7 @@ class GKInputGENE(GKInput, FileReader, file_type="GENE", reads=GKInput):
             references["nref_species"] = "electron"
         else:
             for i_sp in dens_index:
-                if self.data[f"species"][i_sp]["mass"] == 1.0:
+                if np.isclose(self.data[f"species"][i_sp]["mass"], 1.0):
                     references["nref_species"] = references["mref_species"]
 
         if references["nref_species"] is None:
@@ -625,7 +635,7 @@ class GKInputGENE(GKInput, FileReader, file_type="GENE", reads=GKInput):
             references["tref_species"] = "electron"
         else:
             for i_sp in temp_index:
-                if self.data[f"species"][i_sp]["mass"] == 1.0:
+                if np.isclose(self.data[f"species"][i_sp]["mass"], 1.0):
                     references["tref_species"] = references["mref_species"]
 
         if references["nref_species"] is None:
@@ -643,7 +653,11 @@ class GKInputGENE(GKInput, FileReader, file_type="GENE", reads=GKInput):
 
         if references == default_references:
             return {}
+        elif references == pyro_default_references:
+            self.norm_convention = "pyrokinetics"
+            return {}
         else:
+            self.norm_convention = f"{self.code_name.lower()}_bespoke"
             return references
 
     def set(

src/pyrokinetics/gk_code/gs2.py
@@ -506,9 +506,9 @@ class GKInputGS2(GKInput, FileReader, file_type="GS2", reads=GKInput):
             species_key = f"species_parameters_{i_sp + 1}"
 
             # Find all reference values
-            if self.data[species_key]["dens"] == 1.0:
+            if np.isclose(self.data[species_key]["dens"], 1.0):
                 dens_index.append(i_sp)
-            if self.data[species_key]["temp"] == 1.0:
+            if np.isclose(self.data[species_key]["temp"], 1.0):
                 temp_index.append(i_sp)
 
             if (
@@ -532,7 +532,7 @@ class GKInputGS2(GKInput, FileReader, file_type="GS2", reads=GKInput):
         me_md = (electron_mass / deuterium_mass).m
         me_mh = (electron_mass / hydrogen_mass).m
 
-        if e_mass == 1.0:
+        if np.isclose(e_mass, 1.0):
             references["mref_species"] = "electron"
         elif np.isclose(e_mass, me_md, rtol=0.1):
             references["mref_species"] = "deuterium"
@@ -545,7 +545,7 @@ class GKInputGS2(GKInput, FileReader, file_type="GS2", reads=GKInput):
             references["nref_species"] = "electron"
         else:
             for i_sp in dens_index:
-                if self.data[f"species_parameters_{i_sp + 1}"]["mass"] == 1.0:
+                if np.isclose(self.data[f"species_parameters_{i_sp + 1}"]["mass"], 1.0):
                     references["nref_species"] = references["mref_species"]
 
         if references["nref_species"] is None:
@@ -555,7 +555,7 @@ class GKInputGS2(GKInput, FileReader, file_type="GS2", reads=GKInput):
             references["tref_species"] = "electron"
         else:
             for i_sp in temp_index:
-                if self.data[f"species_parameters_{i_sp + 1}"]["mass"] == 1.0:
+                if np.isclose(self.data[f"species_parameters_{i_sp + 1}"]["mass"], 1.0):
                     references["tref_species"] = references["mref_species"]
 
         if references["nref_species"] is None:
@@ -579,6 +579,7 @@ class GKInputGS2(GKInput, FileReader, file_type="GS2", reads=GKInput):
         if references == default_references:
             return {}
         else:
+            self.norm_convention = f"{self.code_name.lower()}_bespoke"
             return references
 
     def set(
@@ -938,10 +939,8 @@ class GKOutputReaderGS2(FileReader, file_type="GS2", reads=GKOutput):
                 )
         gk_input = GKInputGS2()
         gk_input.read_str(input_str)
-        norm_dict = gk_input._get_normalisation()
-        # TODO Need better way to get bespoke norm
-        if norm_dict:
-            gk_input.norm_convention = f"{gk_input.code_name.lower()}_bespoke"
+        gk_input._get_normalisation()
+
         return raw_data, gk_input, input_str
 
     @staticmethod

src/pyrokinetics/gk_code/tglf.py
@@ -456,9 +456,9 @@ class GKInputTGLF(GKInput, FileReader, file_type="TGLF", reads=GKInput):
                 electron_index = i_sp + 1
                 found_electron = True
 
-            if self.data[f"as_{i_sp+1}"] == 1.0:
+            if np.isclose(self.data[f"as_{i_sp+1}"], 1.0):
                 dens_index.append(i_sp+1)
-            if self.data[f"taus_{i_sp+1}"] == 1.0:
+            if np.isclose(self.data[f"taus_{i_sp+1}"], 1.0):
                 temp_index.append(i_sp+1)
 
         if not found_electron:
@@ -477,7 +477,7 @@ class GKInputTGLF(GKInput, FileReader, file_type="TGLF", reads=GKInput):
         me_md = (electron_mass / deuterium_mass).m
         me_mh = (electron_mass / hydrogen_mass).m
 
-        if e_mass == 1.0:
+        if np.isclose(e_mass, 1.0):
             references["mref_species"] = "electron"
         elif np.isclose(e_mass, me_md, rtol=0.1):
             references["mref_species"] = "deuterium"
@@ -490,7 +490,7 @@ class GKInputTGLF(GKInput, FileReader, file_type="TGLF", reads=GKInput):
             references["nref_species"] = "electron"
         else:
             for i_sp in dens_index:
-                if self.data[f"as_{i_sp}"] == 1.0:
+                if np.isclose(self.data[f"as_{i_sp}"], 1.0):
                     references["nref_species"] = references["mref_species"]
 
         if references["nref_species"] is None:
@@ -500,7 +500,7 @@ class GKInputTGLF(GKInput, FileReader, file_type="TGLF", reads=GKInput):
             references["tref_species"] = "electron"
         else:
             for i_sp in temp_index:
-                if self.data[f"taus_{i_sp}"] == 1.0:
+                if np.isclose(self.data[f"taus_{i_sp}"], 1.0):
                     references["tref_species"] = references["mref_species"]
 
         if references["nref_species"] is None:
@@ -516,6 +516,7 @@ class GKInputTGLF(GKInput, FileReader, file_type="TGLF", reads=GKInput):
         if references == default_references:
             return {}
         else:
+            self.norm_convention = f"{self.code_name.lower()}_bespoke"
             return references
 
     def set(
@@ -805,10 +806,8 @@ class GKOutputReaderTGLF(FileReader, file_type="TGLF", reads=GKOutput):
         input_str = raw_data["input"]
         gk_input = GKInputTGLF()
         gk_input.read_str(input_str)
-        norm_dict = gk_input._get_normalisation()
-        # TODO Need better way to get bespoke norm
-        if norm_dict:
-            gk_input.norm_convention = f"{gk_input.code_name.lower()}_bespoke"
+        gk_input._get_normalisation()
+
         return raw_data, gk_input, input_str
 
     @staticmethod