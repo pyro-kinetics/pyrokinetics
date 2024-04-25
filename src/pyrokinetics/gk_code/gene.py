Temp fix to beta/beta_prime in units

src/pyrokinetics/gk_code/gene.py
@@ -249,38 +249,35 @@ class GKInputGENE(GKInput, FileReader, file_type="GENE", reads=GKInput):
         miller_data["ip_ccw"] *= -1
         miller_data["bt_ccw"] *= -1
 
-        # must construct using from_gk_data as we cannot determine bunit_over_b0 here
-        miller = LocalGeometryMiller.from_gk_data(miller_data)
-
         # Assume pref*8pi*1e-7 = 1.0
-        # FIXME Should not be modifying miller after creation
-        ne_norm, Te_norm = self.get_ne_te_normalisation()
-        beta = self.data["general"]["beta"] * ne_norm * Te_norm
+        beta = self.data["general"]["beta"]
         if beta != 0.0:
-            miller.B0 = np.sqrt(1.0 / beta)
+            miller_data["B0"] = np.sqrt(1.0 / beta)
         else:
-            miller.B0 = None
+            miller_data["B0"] = None
 
-        miller.beta_prime = -self.data["geometry"].get("amhd", 0.0) / (
-            miller.q**2 * miller.Rmaj
+        miller_data["beta_prime"] = -self.data["geometry"].get("amhd", 0.0) / (
+            miller_data["q"] ** 2 * miller_data["Rmaj"]
         )
 
         dpdx = self.data["geometry"].get("dpdx_pm", -2)
 
-        if dpdx != -2 and dpdx != -miller.beta_prime:
+        if dpdx != -2 and dpdx != -miller_data["beta_prime"]:
             if dpdx == -1:
                 local_species = self.get_local_species()
-                beta_prime_ratio = -miller.beta_prime / (
+                beta_prime_ratio = -miller_data["beta_prime"] / (
                     local_species.inverse_lp * beta
                 )
                 if not np.isclose(beta_prime_ratio, 1.0):
                     warnings.warn(
-                        "GENE dpdx_pm not set consistently with amhd - drifts may not behave as expected"
+                        "GENE dpdx_pm not set consistently with amhd- drifts may not behave as expected"
                     )
             else:
                 warnings.warn(
                     "GENE dpdx_pm not set consistently with amhd - drifts may not behave as expected"
                 )
+
+        miller = LocalGeometryMiller.from_gk_data(miller_data)
 
         return miller
 
@@ -308,21 +305,18 @@ class GKInputGENE(GKInput, FileReader, file_type="GENE", reads=GKInput):
         miller_data["ip_ccw"] *= -1
         miller_data["bt_ccw"] *= -1
 
-        # must construct using from_gk_data as we cannot determine bunit_over_b0 here
-        miller = LocalGeometryMillerTurnbull.from_gk_data(miller_data)
-
         # Assume pref*8pi*1e-7 = 1.0
-        # FIXME Should not be modifying miller after creation
-        ne_norm, Te_norm = self.get_ne_te_normalisation()
-        beta = self.data["general"]["beta"] * ne_norm * Te_norm
+        beta = self.data["general"]["beta"]
         if beta != 0.0:
-            miller.B0 = np.sqrt(1.0 / beta)
+            miller_data["B0"] = np.sqrt(1.0 / beta)
         else:
-            miller.B0 = None
+            miller_data["B0"] = None
 
-        miller.beta_prime = -self.data["geometry"].get("amhd", 0.0) / (
-            miller.q**2 * miller.Rmaj
+        miller_data["beta_prime"] = -self.data["geometry"].get("amhd", 0.0) / (
+            miller_data["q"] ** 2 * miller_data["Rmaj"]
         )
+
+        miller = LocalGeometryMillerTurnbull.from_gk_data(miller_data)
 
         return miller
 
@@ -344,14 +338,13 @@ class GKInputGENE(GKInput, FileReader, file_type="GENE", reads=GKInput):
             self.data["geometry"].get("trpeps", 0.0) * circular_data["Rmaj"]
         )
 
-        circular = LocalGeometryMillerTurnbull.from_gk_data(circular_data)
-
-        ne_norm, Te_norm = self.get_ne_te_normalisation()
-        beta = self.data["general"]["beta"] * ne_norm * Te_norm
+        beta = self.data["general"]["beta"]
         if beta != 0.0:
-            circular.B0 = np.sqrt(1.0 / beta)
+            circular_data["B0"] = np.sqrt(1.0 / beta)
         else:
-            circular.B0 = None
+            circular_data["B0"] = None
+
+        circular = LocalGeometryMillerTurnbull.from_gk_data(circular_data)
 
         return circular
 

src/pyrokinetics/gk_code/gk_input.py
@@ -307,9 +307,14 @@ class GKInput(AbstractFileReader, ReadableFromFile):
 
         references = copy.copy(default_references)
 
-        references["rgeo_rmaj"] = rgeo_rmaj
-        references["ne"] = electron_density
-        references["te"] = electron_temperature
+        if not np.isclose(rgeo_rmaj, 1.0):
+            references["rgeo_rmaj"] = rgeo_rmaj
+
+        if not np.isclose(electron_density, 1.0):
+            references["ne"] = electron_density
+
+        if not np.isclose(electron_temperature, 1.0):
+            references["te"] = electron_temperature
 
         if not found_electron:
             raise ValueError(

src/pyrokinetics/gk_code/gs2.py
@@ -221,12 +221,7 @@ class GKInputGS2(GKInput, FileReader, file_type="GS2", reads=GKInput):
             self.data["theta_grid_parameters"].get("tripri", 0.0) * rho
         )
 
-        # Get beta and beta_prime normalised to R_major(in case R_geo != R_major)
-        r_geo = self.data["theta_grid_parameters"].get("r_geo", miller_data["Rmaj"])
-
-        ne_norm, Te_norm = self.get_ne_te_normalisation()
-        beta = self._get_beta() * (miller_data["Rmaj"] / r_geo) ** 2 * ne_norm * Te_norm
-        miller_data["beta_prime"] *= (miller_data["Rmaj"] / r_geo) ** 2
+        beta = self._get_beta()
 
         # Assume pref*8pi*1e-7 = 1.0
         miller_data["B0"] = np.sqrt(1.0 / beta) if beta != 0.0 else None
@@ -234,7 +229,6 @@ class GKInputGS2(GKInput, FileReader, file_type="GS2", reads=GKInput):
         miller_data["ip_ccw"] = 1
         miller_data["bt_ccw"] = 1
 
-        # must construct using from_gk_data as we cannot determine bunit_over_b0 here
         return LocalGeometryMiller.from_gk_data(miller_data)
 
     def get_local_species(self):
@@ -304,7 +298,15 @@ class GKInputGS2(GKInput, FileReader, file_type="GS2", reads=GKInput):
         return local_species
 
     def _read_single_grid(self):
-        ky = self.data["kt_grids_single_parameters"]["aky"]
+        try:
+            ky = self.data["kt_grids_single_parameters"]["aky"]
+        except KeyError:
+            ky = (
+                self.data["kt_grids_single_parameters"]["rhostar_single"]
+                * self.data["theta_grid_parameters"]["qinp"]
+                / self.data["theta_grid_parameters"]["rhoc"]
+            )
+
         shat = self.data["theta_grid_eik_knobs"]["s_hat_input"]
         theta0 = self.data["kt_grids_single_parameters"].get("theta0", 0.0)
 
@@ -833,10 +835,9 @@ class GKOutputReaderGS2(FileReader, file_type="GS2", reads=GKOutput):
             self._get_moments(raw_data, gk_input, coords) if load_moments else None
         )
 
-        if fields and coords["linear"]:
-            # Rely on gk_output to generate eigenvalues
-            eigenvalues = None
-        else:
+        eigenvalues = None
+
+        if not fields and coords["linear"]:
             eigenvalues = self._get_eigenvalues(raw_data, coords["time_divisor"])
 
         # Assign units and return GKOutput

src/pyrokinetics/gk_code/tglf.py
@@ -240,25 +240,18 @@ class GKInputTGLF(GKInput, FileReader, file_type="TGLF", reads=GKInput):
 
         miller_data["ip_ccw"] = 1
         miller_data["bt_ccw"] = 1
-        # Must construct using from_gk_data as we cannot determine
-        # bunit_over_b0 here. We also need it to set B0 and
-        # beta_prime, so we have to make a miller instance first
-        miller = LocalGeometryMiller.from_gk_data(miller_data)
 
-        ne_norm, Te_norm = self.get_ne_te_normalisation()
-        beta = self.data.get("betae", 0.0) * ne_norm * Te_norm
-        miller.B0 = 1 / beta**0.5 if beta != 0 else None
+        beta = self.data.get("betae", 0.0)
+        miller_data["B0"] = 1 / beta**0.5 if beta != 0 else None
 
-        # FIXME: This actually needs to be scaled (or overwritten?) by
-        # local_species.inverse_lp and self.data["BETA_STAR_SCALE"]. So we
-        # need to get all the species data first?
-        miller.beta_prime = (
+        miller_data["beta_prime"] = (
             self.data.get("p_prime_loc", 0.0)
             * miller_data["rho"]
             / miller_data["q"]
-            * miller.bunit_over_b0**2
             * (8 * np.pi)
         )
+
+        miller = LocalGeometryMiller.from_gk_data(miller_data)
 
         return miller
 
@@ -278,27 +271,20 @@ class GKInputTGLF(GKInput, FileReader, file_type="TGLF", reads=GKInput):
             self.data.get("q_prime_loc", 16.0) * (mxh_data["rho"] / mxh_data["q"]) ** 2
         )
 
-        # Must construct using from_gk_data as we cannot determine
-        # bunit_over_b0 here. We also need it to set B0 and
-        # beta_prime, so we have to make a mxh instance first
         mxh_data["ip_ccw"] = 1
         mxh_data["bt_ccw"] = 1
 
-        mxh = LocalGeometryMXH.from_gk_data(mxh_data)
+        beta = self.data.get("betae", 0.0)
+        mxh_data["B0"] = 1 / beta**0.5 if beta != 0 else None
 
-        ne_norm, Te_norm = self.get_ne_te_normalisation()
-        beta = self.data.get("betae", 0.0) * ne_norm * Te_norm
-        mxh.B0 = 1 / beta**0.5 if beta != 0 else None
-
-        # FIXME: This actually needs to be scaled (or overwritten?) by
-        # local_species.inverse_lp and self.data["BETA_STAR_SCALE"]. So we
-        # need to get all the species data first?
-        mxh.beta_prime = (
+        mxh_data["beta_prime"] = (
             self.data.get("p_prime_loc", 0.0)
             * mxh_data["rho"]
             / mxh_data["q"]
             * (8 * np.pi)
         )
+
+        mxh = LocalGeometryMXH.from_gk_data(mxh_data)
 
         return mxh
 
@@ -612,7 +598,6 @@ class GKInputTGLF(GKInput, FileReader, file_type="TGLF", reads=GKInput):
             local_geometry.beta_prime
             * local_geometry.q
             / local_geometry.rho
-            / local_geometry.bunit_over_b0**2
             / (8 * np.pi)
         )
 