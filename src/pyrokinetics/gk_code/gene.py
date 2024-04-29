e1ecec62e67ed766a0852ef5b8cf7098e050b40f
bpatel2107 <bpatel2107@users.noreply.github.com>
2024-04-29 14:48:58 +0000

    [skip ci] Apply black changes

--- src/pyrokinetics/gk_code/gene.py
+++ src/pyrokinetics/gk_code/gene.py
@@ -275,15 +275,15 @@ class GKInputGENE(GKInput, FileReader, file_type="GENE", reads=GKInput):
         if dpdx in [-1, -2]:
             local_species = self.get_local_species()
             local_geometry_data["beta_prime"] = (
-                    -local_species.inverse_lp.m
-                    * local_species.pressure.m
-                    / local_geometry_data["B0"] ** 2
+                -local_species.inverse_lp.m
+                * local_species.pressure.m
+                / local_geometry_data["B0"] ** 2
             )
         else:
             local_geometry_data["beta_prime"] = dpdx
 
         amhd_beta_prime = -self.data["geometry"].get("amhd", 0.0) / (
-                local_geometry_data["q"] ** 2 * local_geometry_data["Rmaj"]
+            local_geometry_data["q"] ** 2 * local_geometry_data["Rmaj"]
         )
         if not np.isclose(local_geometry_data["beta_prime"], amhd_beta_prime):
             warnings.warn(
@@ -687,7 +687,6 @@ class GKInputGENE(GKInput, FileReader, file_type="GENE", reads=GKInput):
                     and turnbull_key in self.data["geometry"].keys()
                 ):
                     self.data["geometry"].pop(turnbull_key)
-
 
         self.data["geometry"]["amhd"] = (
             -(local_geometry.q**2) * local_geometry.Rmaj * local_geometry.beta_prime
