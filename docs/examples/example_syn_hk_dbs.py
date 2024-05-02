491856d54d947c60994875c2c007e2550f0bda33
Bhavin Patel <15210802+bpatel2107@users.noreply.github.com>
2024-05-02 19:24:28 +0200

    Tidy up code and plotting

--- docs/examples/example_syn_hk_dbs.py
+++ docs/examples/example_syn_hk_dbs.py
@@ -1,26 +1,26 @@
 from pyrokinetics.diagnostics import SyntheticHighkDBS
 import numpy as np
-import matplotlib.pyplot as plt
 from pyrokinetics.units import ureg
-  
+
 """
 Example file for synthetic diagnostic. Use for producing synthetic frequency/k-spectra from gyrokinetic simulations
 
-Define here inputs characteristic of diagnostic 
-"""  
-  
+Define here inputs characteristic of diagnostic SyntheticHighkDBS
+"""
+
 # inputs
-diag = "highk"                      # 'highk', 'dbs', 'rcdr', 'bes'
-filter_type = ("gauss")             # 'bt_slab', 'bt_scotty', 'gauss' 
-Rloc = 2.89678 * ureg.meter         # 
-Zloc = 0.578291 * ureg.meter        # [m]       
-Kn0_exp = np.asarray([0.5, 1]) / ureg.centimeter      # [cm-1]
-Kb0_exp = np.asarray([0.1, 0.2]) / ureg.centimeter    # [cm-1]
-wR = 0.1 * ureg.meter       #
-wZ = 0.05 * ureg.meter      # 
-eq_file = "/marconi/home/userexternal/jruizrui/files/jet/97090/transp/v04/jet_transp/97090V04.CDF"
+diag = "highk"  # 'highk', 'dbs', 'rcdr', 'bes'
+filter_type = "gauss"  # 'bt_slab', 'bt_scotty', 'gauss'
+Rloc = 2.89678 * ureg.meter  # [m]
+Zloc = 0.578291 * ureg.meter  # [m]
+Kn0_exp = np.asarray([0.5, 1]) / ureg.rhoref_unit  # [cm-1]
+Kb0_exp = np.asarray([0.1, 0.2]) / ureg.rhoref_unit  # [cm-1]
+wR = 0.1 * ureg.meter  #
+wZ = 0.05 * ureg.meter  #
+eq_file = "/marconi_work/FUA38_TURBTAE/97090V04.CDF"
 kinetics_file = eq_file
-simdir = "/marconi_work/FUA37_WPJET1/jruizrui/files/jet/97090/nl/t14.92_rho0.3_phia_deh/ky01_15_lowr"
+simdir = "/marconi_work/FUA38_TURBTAE/ky01_15_lowr"
+gk_file = "input.cgyro"
 savedir = simdir + "/syndiag"
 if_save = 0
 fsize = 22
@@ -37,6 +37,7 @@ syn_diag = SyntheticHighkDBS(
     eq_file=eq_file,
     kinetics_file=kinetics_file,
     simdir=simdir,
+    gk_file=gk_file,
     savedir=simdir,
     fsize=fsize,
 )
@@ -45,6 +46,8 @@ syn_diag = SyntheticHighkDBS(
 syn_diag.mapk()
 
 # apply synthetic diagnostic:
-[pkf, pkf_hann, pkf_kx0ky0, pks, sigma_ks_hann] = syn_diag.get_syn_fspec( 0.7, 1, savedir, if_save )
+[pkf, pkf_hann, pkf_kx0ky0, pks, sigma_ks_hann] = syn_diag.get_syn_fspec(
+    0.7, 1, savedir, if_save
+)
 
 syn_diag.plot_syn()
