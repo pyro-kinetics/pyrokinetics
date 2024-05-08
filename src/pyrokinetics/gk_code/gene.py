af3ef398d672847252a04bc3bfbb1b9b41aea80d
Arkaprava Bokshi <arka.bokshi@york.ac.uk>
2024-05-08 16:38:27 +0100

    add ion counter

--- src/pyrokinetics/gk_code/gene.py
+++ src/pyrokinetics/gk_code/gene.py
@@ -710,6 +710,7 @@ class GKInputGENE(GKInput, FileReader, file_type="GENE", reads=GKInput):
         # Kinetic data
         self.data["box"]["n_spec"] = local_species.nspec
 
+        iIon = 1
         for iSp, name in enumerate(local_species.names):
             try:
                 single_species = self.data["species"][iSp]
@@ -736,7 +737,8 @@ class GKInputGENE(GKInput, FileReader, file_type="GENE", reads=GKInput):
             if name == "electron":
                 single_species["name"] = "electron"
             else:
-                single_species["name"] = f"ion{iSp}"
+                single_species["name"] = f"ion{iIon}"
+                iIon += 1
 
             # TODO Currently forcing GENE to use default pyro. Should check local_norm first
             for key, val in self.pyro_gene_species.items():
