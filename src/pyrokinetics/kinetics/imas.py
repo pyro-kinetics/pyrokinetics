88eb8bb07e07841b27666ca6244feb626012cc0b
Bhavin Patel <15210802+bpatel2107@users.noreply.github.com>
2024-09-10 10:29:06 +0100

    Add IMAS h5 reader in for Kinetics/Equilibrium objects

---
+++ src/pyrokinetics/kinetics/imas.py
@@ -0,0 +1,145 @@
+import h5py
+import numpy as np
+from periodictable import elements
+from pathlib import Path
+
+from ..constants import electron_mass, hydrogen_mass
+from ..file_utils import FileReader
+from ..species import Species
+from ..typing import PathLike
+from ..units import UnitSpline
+from ..units import ureg as units
+from .kinetics import Kinetics
+
+
+class KineticsReaderIMAS(FileReader, file_type="IMAS", reads=Kinetics):
+    def read_from_file(
+        self, filename: PathLike, time_index: int = -1, time: float = None
+    ) -> Kinetics:
+        """
+        Reads in IMAS profiles NetCDF file
+        """
+
+        if time_index is not None and time is not None:
+            raise RuntimeError("Cannot set both 'time' and 'time_index'")
+
+        with h5py.File(filename, "r") as raw_file:
+
+            data = raw_file["core_profiles"]
+
+            time_h5 = data["time"][:]
+            if time_index is None:
+                time_index = -1 if time is None else np.argmin(np.abs(time_h5 - time))
+
+            psi = data["profiles_1d[]&grid&psi"][time_index]
+            psi = psi - psi[0]
+            psi_n = psi / psi[-1] * units.dimensionless
+
+            unit_charge_array = np.ones(len(psi_n))
+
+            rho = psi_n.m
+            rho = rho / rho[-1] * units.lref_minor_radius
+
+            rho_func = UnitSpline(psi_n, rho)
+
+            electron_temp_data = (
+                data["profiles_1d[]&electrons&temperature"][time_index, ...] * units.eV
+            )
+            electron_temp_func = UnitSpline(psi_n, electron_temp_data)
+
+            electron_dens_data = (
+                data["profiles_1d[]&electrons&density_thermal"][time_index, ...]
+                * units.meter**-3
+            )
+            electron_dens_func = UnitSpline(psi_n, electron_dens_data)
+
+            omega_data = electron_dens_data.m * 0.0 * units.second**-1
+
+            omega_func = UnitSpline(psi_n, omega_data)
+
+            electron_charge = UnitSpline(
+                psi_n, -1 * unit_charge_array * units.elementary_charge
+            )
+
+            electron = Species(
+                species_type="electron",
+                charge=electron_charge,
+                mass=electron_mass,
+                dens=electron_dens_func,
+                temp=electron_temp_func,
+                omega0=omega_func,
+                rho=rho_func,
+            )
+
+            result = {"electron": electron}
+
+            # IMAS only has one ion temp
+
+            ion_full_temp_data = (
+                data["profiles_1d[]&ion[]&temperature"][time_index, ...] * units.eV
+            )
+
+            n_ions = ion_full_temp_data.shape[0]
+            unit_array = np.ones(len(psi_n))
+
+            for i_ion in range(n_ions):
+                ion_temp_data = ion_full_temp_data[i_ion, :]
+                ion_temp_func = UnitSpline(psi_n, ion_temp_data)
+
+                ion_dens_data = (
+                    data["profiles_1d[]&ion[]&density"][time_index, i_ion]
+                    / units.meter**3
+                )
+                ion_dens_func = UnitSpline(psi_n, ion_dens_data)
+
+                ion_charge_data = data["profiles_1d[]&ion[]&element[]&z_n"][
+                    time_index, i_ion, 0
+                ]
+                ion_charge_func = UnitSpline(
+                    psi_n, ion_charge_data * unit_array * units.elementary_charge
+                )
+
+                ion_mass = (
+                    data["profiles_1d[]&ion[]&element[]&a"][time_index, i_ion, 0]
+                    * hydrogen_mass
+                )
+                ion_name = data["profiles_1d[]&ion[]&label"][time_index, i_ion].decode(
+                    "utf-8"
+                )
+
+                ion_name = ion_name.split("+")[0]
+                try:
+                    ion_name = getattr(elements, ion_name).name
+                except AttributeError:
+                    ion_name = ion_name
+
+                result[ion_name] = Species(
+                    species_type=ion_name,
+                    charge=ion_charge_func,
+                    mass=ion_mass,
+                    dens=ion_dens_func,
+                    temp=ion_temp_func,
+                    omega0=omega_func,
+                    rho=rho_func,
+                )
+
+        return Kinetics(kinetics_type="IMAS", **result)
+
+    def verify_file_type(self, filename: PathLike) -> None:
+        """Quickly verify that we're looking at a IMAS file without processing"""
+        # Try opening data file
+        # If it doesn't exist or isn't netcdf, this will fail
+        filename = Path(filename)
+        if not filename.is_file():
+            raise FileNotFoundError(filename)
+        try:
+            raw_data = h5py.File(filename, "r")
+        except Exception as exc:
+            raise RuntimeError(
+                "Couldn't read IMAS file. Is the format correct?"
+            ) from exc
+        # Check that the correct variables exist
+        if "equilibrium" not in list(raw_data.keys()):
+            raise ValueError(
+                "IMAS file was missing equilibrium data key. Is the format correct?"
+            )
