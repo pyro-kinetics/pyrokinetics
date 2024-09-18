4b0e41f2acd52bc5badf90ee04cb20dda38c043f
Maurizio Giacomin <maurizio.giacomin@unipd.it>
2024-09-18 10:24:17 +0200

    Add simple tests for the NZT routines

---
+++ tests/diagnostics/test_correlation.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pytest
+from numpy.testing import assert_allclose
+from pathlib import Path
+
+from pyrokinetics import Pyro, template_dir
+from pyrokinetics.diagnostics import Diagnostics
+
+
+def test_displacement():
+    pyro = Pyro(
+        gk_file=template_dir / "outputs" / "CGYRO_nonlinear" / "input.cgyro",
+        gk_code="CGYRO",
+    )
+    pyro.load_gk_output()
+    diag = Diagnostics(pyro)
+    dky = pyro.gk_output["ky"][1]
+    yarray = np.linspace(-np.pi/dky, np.pi/dky, 3)
+    corr = diag.compute_corr_length(1, yarray, Nx=20, ndelta=10)
+    filename = Path(__file__).parent / "golden_answers" / "correlation.npy"
+    data = np.asarray(np.load(filename))
+    assert_allclose(corr, data, rtol=1e-5, atol=1e-8)
