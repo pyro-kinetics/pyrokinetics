d225b103c9b10d1718386f34b48cb889fcc73338
mgiacomin <mgiacomin@users.noreply.github.com>
2024-09-18 08:27:07 +0000

    [skip ci] Apply black changes

--- tests/diagnostics/test_correlation.py
+++ tests/diagnostics/test_correlation.py
@@ -15,7 +15,7 @@ def test_displacement():
     pyro.load_gk_output()
     diag = Diagnostics(pyro)
     dky = pyro.gk_output["ky"][1]
-    yarray = np.linspace(-np.pi/dky, np.pi/dky, 3)
+    yarray = np.linspace(-np.pi / dky, np.pi / dky, 3)
     corr = diag.compute_corr_length(1, yarray, Nx=20, ndelta=10)
     filename = Path(__file__).parent / "golden_answers" / "correlation.npy"
     data = np.asarray(np.load(filename))
