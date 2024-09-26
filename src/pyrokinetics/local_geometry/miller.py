04de9034c870bf7c8d3ff151f238b25990554ced
Liam Pattinson <liampattinson@gmail.com>
2024-09-26 16:42:55 +0100

    Use local theta in Miller fit_params
    Was previously using self.theta_eq

--- src/pyrokinetics/local_geometry/miller.py
+++ src/pyrokinetics/local_geometry/miller.py
@@ -220,7 +220,7 @@ class LocalGeometryMiller(LocalGeometry):
         self.R, self.Z = self.get_flux_surface(theta=self.theta)
 
         params = self.FitParams(shift=shift)
-        fits = self.fit_params(self.theta_eq, b_poloidal, params)
+        fits = self.fit_params(theta, b_poloidal, params)
         self.s_kappa = fits.s_kappa
         self.s_delta = fits.s_delta
         self.shift = fits.shift
