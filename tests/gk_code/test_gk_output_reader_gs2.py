90ba72d8470b86bfe70c448df4a413eff9d37c17
David Dickinson <d.dickinson@york.ac.uk>
2025-01-16 16:18:00 +0000

    Add charge data to gs2 mock_reader in tests

--- tests/gk_code/test_gk_output_reader_gs2.py
+++ tests/gk_code/test_gk_output_reader_gs2.py
@@ -302,6 +302,9 @@ def mock_reader(monkeypatch, request):
                 np.ones(gs2_field_shape),
             )
 
+        data_vars["charge"] = [self.data[f"species_parameters_{num+1}"]["z"]
+                               for num in range(self.data["species_knobs"]["nspec"])]
+
         moments = ["part", "heat", "mom"]
         for field, moment in product(fields, moments):
             flux_field = "es" if field == "phi" else field
