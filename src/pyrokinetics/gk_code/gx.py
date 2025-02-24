5f96e8e739385663afaca631aa24a3012486636c
physycola <physycola@users.noreply.github.com>
2025-02-24 18:15:32 +0000

    [skip ci] Apply black changes

--- src/pyrokinetics/gk_code/gx.py
+++ src/pyrokinetics/gk_code/gx.py
@@ -424,7 +424,9 @@ class GKInputGX(GKInput, FileReader, file_type="GX", reads=GKInput):
         delta_time = self.data["Time"].get("dt", 0.05)
         numerics_data["delta_time"] = delta_time
         if "nstep" in self.data["Time"]:
-            numerics_data["max_time"] = self.data["Time"].get("nstep", 10000) * delta_time
+            numerics_data["max_time"] = (
+                self.data["Time"].get("nstep", 10000) * delta_time
+            )
         else:
             numerics_data["max_time"] = self.data["Time"].get("t_max", 1000.0)
 
