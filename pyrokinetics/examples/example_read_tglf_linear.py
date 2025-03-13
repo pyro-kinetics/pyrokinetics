58e6c4b7d75b6df543dcf7144f9ba014d3635225
fcasson <fcasson@users.noreply.github.com>
2025-03-13 23:31:43 +0000

    [skip ci] Apply black changes

--- pyrokinetics/examples/example_read_tglf_linear.py
+++ pyrokinetics/examples/example_read_tglf_linear.py
@@ -8,39 +8,41 @@ pyro.load_gk_output()
 
 # Plot dominant eigenfunction
 try:
-  dominant = pyro.gk_output.data["eigenfunctions"].isel(mode=0)
+    dominant = pyro.gk_output.data["eigenfunctions"].isel(mode=0)
 
-  np.real(dominant.sel(field="phi")).plot(x="theta", marker="x")
-  np.imag(dominant.sel(field="phi")).plot(x="theta", marker="o")
-  plt.show()
+    np.real(dominant.sel(field="phi")).plot(x="theta", marker="x")
+    np.imag(dominant.sel(field="phi")).plot(x="theta", marker="o")
+    plt.show()
 
-  np.real(dominant.sel(field="apar")).plot(marker="x")
-  np.imag(dominant.sel(field="apar")).plot(marker="o")
-  plt.show()
+    np.real(dominant.sel(field="apar")).plot(marker="x")
+    np.imag(dominant.sel(field="apar")).plot(marker="o")
+    plt.show()
 except:
-  print('no eigenfunctions present, rerun TGLF with WRITE_WAVEFUNCTION_FLAG=T and USE_TRANSPORT_MODEL=F ??')
+    print(
+        "no eigenfunctions present, rerun TGLF with WRITE_WAVEFUNCTION_FLAG=T and USE_TRANSPORT_MODEL=F ??"
+    )
 
 # Plot subdominant eigenfunction
 try:
-  subdominant = pyro.gk_output.data["eigenfunctions"].isel(mode=1)
-  np.real(subdominant.sel(field="phi")).plot(marker="x")
-  np.imag(subdominant.sel(field="phi")).plot(marker="o")
-  plt.show()
+    subdominant = pyro.gk_output.data["eigenfunctions"].isel(mode=1)
+    np.real(subdominant.sel(field="phi")).plot(marker="x")
+    np.imag(subdominant.sel(field="phi")).plot(marker="o")
+    plt.show()
 
-  np.real(subdominant.sel(field="apar")).plot(marker="x")
-  np.imag(subdominant.sel(field="apar")).plot(marker="o")
-  plt.show()
+    np.real(subdominant.sel(field="apar")).plot(marker="x")
+    np.imag(subdominant.sel(field="apar")).plot(marker="o")
+    plt.show()
 except:
-  print('no eignenfunctions present, rerun TGLF with WRITE_WAVEFUNCTION_FLAG=T')
+    print("no eignenfunctions present, rerun TGLF with WRITE_WAVEFUNCTION_FLAG=T")
 
 # Plot growth rate and frequency
 growth_rate = pyro.gk_output.data["growth_rate"]
-growth_rate[:,0].plot()
-growth_rate[:,1].plot()
+growth_rate[:, 0].plot()
+growth_rate[:, 1].plot()
 
 plt.show()
 
 mode_frequency = pyro.gk_output.data["mode_frequency"]
-mode_frequency[:,0].plot()
-mode_frequency[:,1].plot()
+mode_frequency[:, 0].plot()
+mode_frequency[:, 1].plot()
 plt.show()
