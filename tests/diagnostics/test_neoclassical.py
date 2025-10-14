d2bb86def790331cdfa7743b45d5ddfc349cdff2
Bhavin Patel <15210802+bpatel2107@users.noreply.github.com>
2025-10-14 15:18:50 +0100

    Add test for Jtot.B

--- tests/diagnostics/test_neoclassical.py
+++ tests/diagnostics/test_neoclassical.py
@@ -12,21 +12,24 @@ def test_bootstrap_current():
     scene_cdf = template_dir / "scene.cdf"
     scene_eqdsk = template_dir / "step.geqdsk"
 
-    downsize = 5
     scene_data = nc.Dataset(scene_cdf)
-    scene_psin = scene_data["rho_psi"][::-downsize] ** 2
+    indices = slice(10, -10, 2)
+    scene_psin = scene_data["rho_psi"][indices] ** 2
 
     # SCENE gives <Jbs. B> / <B^2>
-    scene_jdotb_b2 = (
-        scene_data["Jbs.B"][::-downsize].data
-        * units.ampere
-        / units.tesla
-        / units.meter**2
+    scene_jbsdotb_b2 = (
+        scene_data["Jbs.B"][indices].data * units.ampere / units.tesla / units.meter**2
     )
-    scene_zeff = scene_data["zeff"][::-downsize].data * units.elementary_charge
+    scene_zeff = scene_data["zeff"][indices].data * units.elementary_charge
 
-    redl_jdotb_b2 = scene_jdotb_b2 * 0.0
-    sauter_jdotb_b2 = scene_jdotb_b2 * 0.0
+    scene_jtotdotb_b2 = (
+        scene_data["Jtot.B"][indices].data * units.ampere / units.tesla / units.meter**2
+    )
+
+    redl_jbsdotb_b2 = scene_jbsdotb_b2 * 0.0
+    sauter_jbsdotb_b2 = scene_jbsdotb_b2 * 0.0
+    redl_jtotdotb_b2 = scene_jtotdotb_b2 * 0.0
+    sauter_jtotdotb_b2 = scene_jtotdotb_b2 * 0.0
 
     # Load up pyro object
     pyro = Pyro(
@@ -36,21 +39,27 @@ def test_bootstrap_current():
         kinetics_type="SCENE",
     )
 
-    for i, psi_n in enumerate(scene_psin[1:]):
+    for i, psi_n in enumerate(scene_psin):
         try:
             pyro.load_local(psi_n=psi_n, local_geometry="MXH")
         except Exception:
             continue
 
-        pyro.local_species.zeff = scene_zeff[i + 1]
+        pyro.local_species.zeff = scene_zeff[i]
 
         redl = Redl2021(pyro)
-        redl_jdotb_b2[i + 1] = (redl.JbsdotB / redl.B2_fsa).to("ampere / tesla / m**2")
+        redl_jbsdotb_b2[i] = (redl.JbsdotB / redl.B2_fsa).to("ampere / tesla / m**2")
+        redl_jtotdotb_b2[i] = (redl.JdotB / redl.B2_fsa).to("ampere / tesla / m**2")
 
         sauter = Sauter1999(pyro)
-        sauter_jdotb_b2[i + 1] = (sauter.JbsdotB / sauter.B2_fsa).to(
+        sauter_jbsdotb_b2[i] = (sauter.JbsdotB / sauter.B2_fsa).to(
+            "ampere / tesla / m**2"
+        )
+        sauter_jtotdotb_b2[i] = (sauter.JdotB / sauter.B2_fsa).to(
             "ampere / tesla / m**2"
         )
 
-    assert_allclose(redl_jdotb_b2.m, scene_jdotb_b2.m, rtol=6e-2)
-    assert_allclose(sauter_jdotb_b2.m, scene_jdotb_b2.m, rtol=15e-2)
+    assert_allclose(redl_jbsdotb_b2.m, scene_jbsdotb_b2.m, rtol=6e-2)
+    assert_allclose(sauter_jbsdotb_b2.m, scene_jbsdotb_b2.m, rtol=15e-2)
+    assert_allclose(redl_jtotdotb_b2.m, scene_jtotdotb_b2.m, rtol=1e-2)
+    assert_allclose(sauter_jtotdotb_b2.m, scene_jtotdotb_b2.m, rtol=1e-2)
