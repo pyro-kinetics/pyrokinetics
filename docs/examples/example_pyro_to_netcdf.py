510643ad0c6f377197b629deaf01d99aea676e96
FelixWattsYork <hmq514@york.ac.uk>
2026-02-13 16:34:38 +0000

    black fix

--- docs/examples/example_pyro_to_netcdf.py
+++ docs/examples/example_pyro_to_netcdf.py
@@ -1,4 +1,4 @@
-from pyrokinetics import pyro, template_dir
+from pyrokinetics import Pyro, template_dir
 
 # Saving pyro gk outputs to netcdf files and loading from then can save time
 # especially if loading large files
@@ -10,3 +10,4 @@ pyro.gk_output.to_netcdf(path / "linear_cgyro.nc")
 
 new_pyro = Pyro(gk_file=path / "input.cgyro")
 new_pyro.load_gk_output(netcdf_file=path / "linear_cgyro.nc")
+
