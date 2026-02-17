cd337d6d64bee381e6ccc1a6522aa4d82a449cd2
Bhavin Patel <15210802+bpatel2107@users.noreply.github.com>
2026-02-17 10:20:54 +0000

    Update example and docs

--- docs/examples/example_pyro_to_netcdf.py
+++ docs/examples/example_pyro_to_netcdf.py
@@ -5,8 +5,9 @@ from pyrokinetics import Pyro, template_dir
 
 path = template_dir / "outputs" / "CGYRO_linear"
 pyro = Pyro(gk_file=path / "input.cgyro")
+
 pyro.load_gk_output()
-pyro.gk_output.to_netcdf(path / "linear_cgyro.nc")
+pyro.gk_output.to_netcdf("linear_cgyro.nc")
 
 new_pyro = Pyro(gk_file=path / "input.cgyro")
-new_pyro.load_gk_output(netcdf_file=path / "linear_cgyro.nc")
+new_pyro.load_gk_output(netcdf_file="linear_cgyro.nc")
