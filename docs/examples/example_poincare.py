1ac2ec0613b2ab781c921dd6184b937a2f4ace85
Bhavin Patel <15210802+bpatel2107@users.noreply.github.com>
2026-03-05 12:25:54 +0000

    Update field line following diagnostics

--- docs/examples/example_poincare.py
+++ docs/examples/example_poincare.py
@@ -10,8 +10,8 @@ pyro = Pyro(gk_file=fname, gk_code="CGYRO")
 pyro.load_gk_output()
 
 # Set input for Poincare map
-xarray = np.linspace(6, 8, 5)
-yarray = np.linspace(-10, 10, 3)
+xarray = np.linspace(6, 8, 5) * pyro.norms.rhoref
+yarray = np.linspace(-10, 10, 3) * pyro.norms.rhoref
 nturns = 1000
 time = 1
 rhostar = 0.036
@@ -22,12 +22,22 @@ coords = diag.poincare(xarray, yarray, nturns, time, rhostar)
 
 # Simple plot
 plt.figure()
-plt.plot(coords[0, :].ravel(), coords[1, :].ravel(), "k.")
+plt.plot(coords[0, :].ravel().m, coords[1, :].ravel().m, "k.")
 
 # Plot with colors
 ntot = nturns * yarray.shape[0]
 colorlist = plt.cm.jet(np.linspace(0, 1, xarray.shape[0]))
 plt.figure()
 for i, color in enumerate(colorlist):
-    plt.plot(coords[0, :, :, i].ravel(), coords[1, :, :, i].ravel(), ".", color=color)
+    plt.plot(coords[0, :, :, i].ravel().m, coords[1, :, :, i].ravel().m, ".", color=color)
 plt.show()
+
+
+radial_diff = diag.radial_diffusion_coefficient(xarray, yarray, nturns, time, rhostar)
+print("Radial diffusion", radial_diff)
+
+delta_r = diag.compute_half_displacement(xarray, yarray, time, rhostar)
+print("Avg delta r", np.mean(delta_r))
+
+lambda_Bxx = diag.parallel_correlation_length(time)
+print("My lambda Bxx", lambda_Bxx)
