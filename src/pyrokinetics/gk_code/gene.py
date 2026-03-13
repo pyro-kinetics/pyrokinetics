fixed incorrect intopolation of jacobian for GENE

src/pyrokinetics/gk_code/gene.py
@@ -1084,7 +1084,6 @@ class GKInputGENE(GKInput, FileReader, file_type="GENE", reads=GKInput):
 
         # Load each species into a dictionary
         for i_sp in range(self.data["box"]["n_spec"]):
-
             try:
                 gene_data = self.data["species"][i_sp]
             except TypeError:
@@ -1813,12 +1812,19 @@ class GKOutputReaderGENE(FileReader, file_type="GENE", reads=GKOutput):
         ntheta = nz
         local_geometry = gk_input.get_local_geometry()
         metric_terms = MetricTerms(local_geometry, ntheta=nz * 4)
-        Jacobian = metric_terms.Jacobian
         z_full = metric_terms.alpha / local_geometry.q
 
         theta = np.interp(z, z_full, metric_terms.regulartheta)
         nenergy = nml["box"]["nv0"]
         energy = np.linspace(-1, 1, nenergy)
+        theta_mod = np.mod(theta, 2 * np.pi)
+
+        Jacobian = np.interp(
+            theta_mod,
+            metric_terms.regulartheta,
+            metric_terms.Jacobian,
+            period=2 * np.pi,
+        )
 
         npitch = nml["box"]["nw0"]
         pitch = np.linspace(-1, 1, npitch)
@@ -2388,7 +2394,6 @@ class GKOutputReaderGENE(FileReader, file_type="GENE", reads=GKOutput):
                     for i_field in range(nfield):
                         for i_flux in range(len(coords["flux"])):
                             if i_flux == 2 and prefixes[-1] == "P_t":
-
                                 # Sum over contributions
                                 flux_data = sum(
                                     (