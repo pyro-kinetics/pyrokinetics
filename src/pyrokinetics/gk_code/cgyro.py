Fix jacobian to use 4 * ntheta resolution always

src/pyrokinetics/gk_code/cgyro.py
@@ -1355,7 +1355,7 @@ class GKOutputReaderCGYRO(FileReader, file_type="CGYRO", reads=GKOutput):
             time = time[downsample.get("time_idx", slice(None))]
 
         local_geometry = gk_input.get_local_geometry()
-        metric_terms = MetricTerms(local_geometry, ntheta=ntheta * 4)
+        metric_terms = MetricTerms(local_geometry, ntheta=ntheta_grid * 4)
         theta_mod = np.mod(theta, 2 * np.pi)
         Jacobian = np.interp(
             theta_mod,

src/pyrokinetics/gk_code/gs2.py
@@ -1301,7 +1301,8 @@ class GKOutputReaderGS2(FileReader, file_type="GS2", reads=GKOutput):
                 species.append(f"ion{ion_num}")
 
         local_geometry = gk_input.get_local_geometry()
-        metric_terms = MetricTerms(local_geometry, ntheta=len(theta) * 4)
+        ntheta_metric = gk_input.data["theta_grid_parameters"]["ntheta"]
+        metric_terms = MetricTerms(local_geometry, ntheta=ntheta_metric * 4)
         theta_mod = np.mod(theta, 2 * np.pi)
         Jacobian = np.interp(
             theta_mod,

src/pyrokinetics/gk_code/gx.py
@@ -1185,7 +1185,8 @@ class GKOutputReaderGX(FileReader, file_type="GX", reads=GKOutput):
             )
 
         local_geometry = gk_input.get_local_geometry()
-        metric_terms = MetricTerms(local_geometry, ntheta=len(theta) * 4)
+        metric_ntheta = gk_input.data["Dimensions"]["ntheta"]
+        metric_terms = MetricTerms(local_geometry, ntheta=metric_ntheta * 4)
         theta_mod = np.mod(theta, 2 * np.pi)
         Jacobian = np.interp(
             theta_mod,